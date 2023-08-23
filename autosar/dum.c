1d6619dda931502cd5de03b1e64f2cd7f7bd23d5
DiegoMX <diego.perez@modular-mx.com>
2023-08-22 20:53:04 -0700

    added dum test to fix and validate unit tes set-up

--- autosar/dum.c
+++ autosar/dum.c
@@ -1,3 +1,10 @@
 /*This is a workaround file so cppcheck does not complain about autosar directory
 not having nothing to lint
 MUST be renmove once a source .c file is added to any of the subdirectories*/
+#include <stdint.h>
+#include "dum.h"
+
+uint32_t sum( uint32_t a, uint32_t b )
+{
+    return a + b;
+}
