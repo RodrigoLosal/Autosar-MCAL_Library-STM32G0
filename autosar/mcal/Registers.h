d29c5ccb095ca7ab77cad681e537c3d018f7b2fa
JAZ <jazmin.melendrez33@gmail.com>
2023-08-24 14:59:33 -0600

    It was added the RESERVED memory in the NVIC_Type structure

--- autosar/mcal/Registers.h
+++ autosar/mcal/Registers.h
@@ -148,10 +148,15 @@ typedef struct
 typedef struct
 {
     uint32 ISER[ 1U ]; /*!< Offset: 0x000 (R/W)  Interrupt Set Enable Register */
+    uint32 RESERVED0[ 31U ];
     uint32 ICER[ 1U ]; /*!< Offset: 0x080 (R/W)  Interrupt Clear Enable Register */
+    uint32 RESERVED1[ 31U ];
     uint32 ISPR[ 1U ]; /*!< Offset: 0x100 (R/W)  Interrupt Set Pending Register */
+    uint32 RESERVED2[ 31U ];
     uint32 ICPR[ 1U ]; /*!< Offset: 0x180 (R/W)  Interrupt Clear Pending Register */
-    uint32 IP[ 8U ];   /*!< Offset: 0x300 (R/W)  Interrupt Priority Register */
+    uint32 RESERVED3[ 31U ];
+    uint32 RESERVED4[ 64U ];
+    uint32 IP[ 8U ]; /*!< Offset: 0x300 (R/W)  Interrupt Priority Register */
 } NVIC_Type;
 
 #endif
