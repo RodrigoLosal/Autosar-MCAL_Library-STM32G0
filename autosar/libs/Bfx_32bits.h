4a2bc34024971a76368450db59a421a327dacbbd
Enrique <joenorlu@gmail.com>
2023-08-28 17:34:36 -0600

    Changes

--- autosar/libs/Bfx_32bits.h
+++ autosar/libs/Bfx_32bits.h
@@ -53,7 +53,7 @@ static inline void Bfx_ClrBitMask_u32u32( uint32 *Data, uint32 Mask )
 static inline boolean Bfx_TstBitMask_u32u32_u8( uint32 Data, uint32 Mask )
 {
     boolean Result;
-    
+
     Result = ( Data & Mask ) == Mask;
     return Result;
 }
@@ -61,7 +61,7 @@ static inline boolean Bfx_TstBitMask_u32u32_u8( uint32 Data, uint32 Mask )
 static inline boolean Bfx_TstBitLnMask_u32u32_u8( uint32 Data, uint32 Mask )
 {
     boolean Result;
-    
+
     Result = ( Data & Mask ) > 0;
     return Result;
 }
@@ -128,7 +128,7 @@ static inline void Bfx_CopyBit_u32u8u32u8( uint32 *DestinationData, uint8 Destin
 static inline void Bfx_PutBits_u32u8u8u32( uint32 *Data, uint8 BitStartPn, uint8 BitLn, uint32 Pattern )
 {
     uint32 Mask = ( ( 1u << BitLn ) - 1u ) << BitStartPn;
-    *Data      = ( *Data & ~Mask ) | ( ( Pattern << BitStartPn ) & Mask );
+    *Data       = ( *Data & ~Mask ) | ( ( Pattern << BitStartPn ) & Mask );
 }
 
 static inline void Bfx_PutBitsMask_u32u32u32( uint32 *Data, uint32 Pattern, uint32 Mask )
