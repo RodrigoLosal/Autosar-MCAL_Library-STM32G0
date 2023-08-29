22ec5c45ca8a897338c9f6949f2e2b9664a0286d
FlavioCJM1 <flaviocjm@outlook.es>
2023-08-29 16:45:55 -0600

    Used Std_types library instead Platform_types

--- autosar/Crc.h
+++ autosar/Crc.h
@@ -1,9 +1,7 @@
 #ifndef CRC_H_
 #define CRC_H_
 
-#include "Platform_Types.h"
-#include <stdio.h>
-#include <stdint.h>
+#include "Std_Types.h"
 
 uint8 Crc_CalculateCRC8( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8, boolean Crc_IsFirstCall );
 uint8 Crc_CalculateCRC8H2F( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8H2F, boolean Crc_IsFirstCall );
