f5ec326f16378194cbead5b0411851cd0aea9854
JAZ <jazmin.melendrez33@gmail.com>
2023-08-30 00:00:23 -0600

    some definitions were moved from Registers.h to Nvic.h file

--- autosar/mcal/Nvic.h
+++ autosar/mcal/Nvic.h
@@ -4,6 +4,10 @@
 #include "Registers.h" // Incluir las definiciones de los registros del microcontrolador
 #include "Platform_Types.h"
 
+#define _BIT_SHIFT( irq ) ( ( ( ( (uint32)irq ) ) & 0x03UL ) * 8UL )
+#define _IP_IDX( irq )    ( ( ( (uint32)irq ) >> 2UL ) )
+#define NVIC_MIN_IRQ     16U
+#define NVIC_MAX_IRQ     30U
 typedef enum
 {
     /******  Cortex-M0+ Processor Exceptions Numbers ***************************************************************/
