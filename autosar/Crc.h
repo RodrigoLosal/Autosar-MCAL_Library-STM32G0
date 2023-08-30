e349654b488adbe7c872cbd6f58826a070cf7164
FlavioCJM1 <flaviocjm@outlook.es>
2023-08-30 11:19:52 -0600

    Macros were moved to .c file

--- autosar/Crc.h
+++ autosar/Crc.h
@@ -3,30 +3,6 @@
 
 #include "Std_Types.h"
 
-#define CRC8             0x1D
-#define FIRSTCALLCRC8    0xFF
-#define CRC8NBITS        8
-#define CRC8MSB          0x80
-
-#define CRC8H2F          0x2F
-#define FIRSTCALLCRC8H2F 0xFF
-#define CRC8H2FNBITS     8
-#define CRC8H2NMSB       0x80
-
-#define CRC16            0x1021
-#define FIRSTCALLCRC16   0xFFFF
-#define CRC16NBITS       8
-#define CRC16MSB         0x8000
-#define CRC16_8LEFT      8
-
-#define REFLECTLSB       0x01
-
-#define VENDOR_ID   0x0000
-#define MODULE_ID   0x0000
-#define CRC_SW_MAJOR_VERSION 0
-#define CRC_SW_MINOR_VERSION 0
-#define CRC_SW_PATCH_VERSION 0
-
 uint8 Crc_CalculateCRC8( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8, boolean Crc_IsFirstCall );
 uint8 Crc_CalculateCRC8H2F( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8H2F, boolean Crc_IsFirstCall );
 uint16 Crc_CalculateCRC16( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint16 Crc_StartValue16, boolean Crc_IsFirstCall );
