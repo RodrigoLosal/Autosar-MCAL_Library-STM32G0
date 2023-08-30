98ad9c5bae9418cec34422f8f30128aaa74d0f07
FlavioCJM1 <flaviocjm@outlook.es>
2023-08-30 11:32:22 -0600

    Was moved the prototype of function to .c file

--- autosar/Crc.h
+++ autosar/Crc.h
@@ -5,6 +5,5 @@ uint8 Crc_CalculateCRC8( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_
 uint8 Crc_CalculateCRC8H2F( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8H2F, boolean Crc_IsFirstCall );
 uint16 Crc_CalculateCRC16( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint16 Crc_StartValue16, boolean Crc_IsFirstCall );
 void Crc_GetVersionInfo( Std_VersionInfoType *Versioninfo );
-uint32 Reflect( uint32 data, uint8 bit_count );
 
 #endif
