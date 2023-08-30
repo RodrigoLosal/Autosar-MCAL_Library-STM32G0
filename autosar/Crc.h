35709e5396e5f3cd18bd125bd61cd05d7925ddfe
FlavioCJM1 <flaviocjm@outlook.es>
2023-08-30 09:54:51 -0600

    Added the macros for versioninfo function

--- autosar/Crc.h
+++ autosar/Crc.h
@@ -21,9 +21,16 @@
 
 #define REFLECTLSB       0x01
 
+#define VENDOR_ID   0x0000
+#define MODULE_ID   0x0000
+#define CRC_SW_MAJOR_VERSION 0
+#define CRC_SW_MINOR_VERSION 0
+#define CRC_SW_PATCH_VERSION 0
+
 uint8 Crc_CalculateCRC8( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8, boolean Crc_IsFirstCall );
 uint8 Crc_CalculateCRC8H2F( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8H2F, boolean Crc_IsFirstCall );
 uint16 Crc_CalculateCRC16( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint16 Crc_StartValue16, boolean Crc_IsFirstCall );
+void Crc_GetVersionInfo(Std_VersionInfoType* Versioninfo);
 uint32 Reflect( uint32 data, uint8 bit_count );
 
 #endif
