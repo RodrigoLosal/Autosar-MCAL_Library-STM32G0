d9db6e6cbc5ed3501fc81e1efc30a1a8c41a58c2
FlavioCJM1 <flaviocjm@outlook.es>
2023-08-30 11:22:12 -0600

    Was removed the Std_Types Header to put on .c file

--- autosar/Crc.h
+++ autosar/Crc.h
@@ -1,12 +1,10 @@
 #ifndef CRC_H_
 #define CRC_H_
 
-#include "Std_Types.h"
-
 uint8 Crc_CalculateCRC8( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8, boolean Crc_IsFirstCall );
 uint8 Crc_CalculateCRC8H2F( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_StartValue8H2F, boolean Crc_IsFirstCall );
 uint16 Crc_CalculateCRC16( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint16 Crc_StartValue16, boolean Crc_IsFirstCall );
-void Crc_GetVersionInfo(Std_VersionInfoType* Versioninfo);
+void Crc_GetVersionInfo( Std_VersionInfoType *Versioninfo );
 uint32 Reflect( uint32 data, uint8 bit_count );
 
 #endif
