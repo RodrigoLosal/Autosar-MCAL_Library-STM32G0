e7f26536cdf30233b818c2c06f9844da2a7ada87
JAZ <jazmin.melendrez33@gmail.com>
2023-08-30 12:37:35 -0600

    definitions were relocated

--- autosar/mcal/Nvic.h
+++ autosar/mcal/Nvic.h
@@ -1,12 +1,7 @@
 #ifndef NVIC_H
 #define NVIC_H
 
-#include "Platform_Types.h"
-
-#define _BIT_SHIFT( irq ) ( ( ( ( (uint32)irq ) ) & 0x03UL ) * 8UL )
-#define _IP_IDX( irq )    ( ( ( (uint32)irq ) >> 2UL ) )
-#define NVIC_MIN_IRQ      16U
-#define NVIC_MAX_IRQ      30U
+#include "Std_Types.h"
 
 void CDD_Nvic_SetPriority( Nvic_IrqType irq, uint32 priority );
 uint32 CDD_Nvic_GetPriority( Nvic_IrqType irq );
