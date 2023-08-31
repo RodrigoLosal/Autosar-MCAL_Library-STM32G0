135db879235004c9c2bfe4d55a10169e3e2db4a2
Daniel Mendivil Byerly <danielmbyerly@hotmail.com>
2023-08-31 14:36:14 -0700

    rebase

--- test/test_Bfx_8bits.c
+++ test/test_Bfx_8bits.c
@@ -19,7 +19,11 @@ void setUp( void )
 {
 }
 
-/*this function is required by Ceedling to run any code after the test cases*/
+/**
+ * @brief   **tearDown**
+ *
+ * This function is required by Ceedling to run any code before the test cases.
+ */
 void tearDown( void )
 {
 }
@@ -593,7 +597,7 @@ void test__CountLeadingSigns_s8__2zeros( void )
  *
  * The test validates if function counts 6 zeros from left to right
  */
-void test__Bfx_CountLeadingZeros_u8__6ceros( void )
+void test__Bfx_CountLeadingZeros_u8__6zeros( void )
 {
     uint8 Data   = 0x03;
     uint8 Result = Bfx_CountLeadingZeros_u8( Data );
@@ -605,7 +609,7 @@ void test__Bfx_CountLeadingZeros_u8__6ceros( void )
  *
  * The test validates if function counts 1 zero from left to right
  */
-void test__Bfx_CountLeadingZeros_u8__1ceros( void )
+void test__Bfx_CountLeadingZeros_u8__1zero( void )
 {
     uint8 Data   = 0x40;
     uint8 Result = Bfx_CountLeadingZeros_u8( Data );
