6fcf571966b8b7316435d484446907ad38f82c5b
Daniel Mendivil Byerly <danielmbyerly@hotmail.com>
2023-09-01 14:17:38 -0700

    adding config files for the port library

---
+++ autosar/Cfg/Port_Cfg.h
@@ -0,0 +1,16 @@
+#ifndef PORT_CFG_H_
+#define PORT_CFG_H_
+
+#define PORT_PIN_NUMBER_OF_PORTS     1
+
+/*pins to use in our application where port is represented by the
+most significant nibble and pin by the less isgnificant nibble*/
+
+#define PORT_SET_PIN_DIRECTION_API      TRUE
+#define PORT_SET_PIN_MODE_API           TRUE
+#define PORT_VERSION_INFO_API           TRUE
+
+/*external reference to Port_Config array*/
+extern const Port_ConfigType PortConfig[PORT_PIN_NUMBER_OF_PORTS];
+
+#endif /* PORT_CFG_H_ */
