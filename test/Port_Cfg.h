0a39308146951f712674c61fa6207f185eed63fc
Daniel Mendivil Byerly <danielmbyerly@hotmail.com>
2023-09-04 15:54:27 -0700

    adding extra cfg file

---
+++ test/Port_Cfg.h
@@ -0,0 +1,14 @@
+#ifndef PORT_CFG_H_
+#define PORT_CFG_H_
+
+#define PORT_PIN_NUMBER_OF_PORTS   3
+
+/*pins to use in our application where port is represented by the
+most significant nibble and pin by the less isgnificant nibble*/
+
+#define PORT_SET_PIN_DIRECTION_API STD_ON
+#define PORT_SET_PIN_MODE_API      STD_ON
+#define PORT_VERSION_INFO_API      STD_ON
+
+
+#endif /* PORT_CFG_H_ */
