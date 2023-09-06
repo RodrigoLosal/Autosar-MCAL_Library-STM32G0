b2ad46fd541fa7248f431887b3ce9bffeaa2e3a5
Daniel Mendivil Byerly <danielmbyerly@hotmail.com>
2023-09-06 10:12:46 -0700

    adding Port_Cfg.c for const declaration

--- test/support/Port_Cfg.h
+++ test/support/Port_Cfg.h
@@ -10,5 +10,6 @@ most significant nibble and pin by the less isgnificant nibble*/
 #define PORT_SET_PIN_MODE_API      STD_ON
 #define PORT_VERSION_INFO_API      STD_ON
 
+extern const Port_ConfigType PortsConfig[ PORT_PIN_NUMBER_OF_PORTS ];
 
 #endif /* PORT_CFG_H_ */
--- test/test_Port.c
+++ test/test_Port.c
@@ -11,46 +11,8 @@ Port_RegisterType PORTD_BASE = { 0xFFFFFFFF, 0x00, 0x00000000, 0x00000000, 0x00,
 Port_RegisterType PORTE_BASE = { 0xFFFFFFFF, 0x00, 0x00000000, 0x00000000, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 Port_RegisterType PORTF_BASE = { 0xFFFFFFFF, 0x00, 0x00000000, 0x00000000, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-/*redefine the port numbers from Port_Cfg.h*/
-
-
 #include "Port.h"
-
-/*set the ports and pins and its corresponding values to test*/
-const Port_ConfigType PortsConfig[ PORT_PIN_NUMBER_OF_PORTS ] =
-{
-{ .Port          = PORTS_C,
-  .Pins          = PORTS_PIN_0 | PORTS_PIN_1,
-  .Pull          = PORTS_NOPULL,
-  .OutputDrive   = PORTS_PUSH_PULL,
-  .Speed         = PORTS_LOW_SPEED,
-  .Mode          = PORTS_MODE_OUTPUT,
-  .Altern        = 0,
-  .Pin_direction = PORTS_CHANGEABLE },
-{ .Port          = PORTS_A,
-  .Pins          = PORTS_PIN_4 | PORTS_PIN_5,
-  .Pull          = PORTS_PULLDOWN,
-  .OutputDrive   = PORTS_OPEN_COLECTOR,
-  .Speed         = PORTS_VERY_HIGH_SPEED,
-  .Mode          = PORTS_MODE_OUTPUT,
-  .Altern        = 0,
-  .Pin_direction = PORTS_NON_CHANGEABLE },
-{ .Port          = PORTS_B,
-  .Pins          = PORTS_PIN_13 | PORTS_PIN_10 | PORTS_PIN_5,
-  .Pull          = PORTS_PULLDOWN,
-  .OutputDrive   = PORTS_OPEN_COLECTOR,
-  .Speed         = PORTS_VERY_HIGH_SPEED,
-  .Mode          = PORTS_MODE_ALT,
-  .Altern        = PORT_PIN_MODE_AF2,
-  .Pin_direction = PORTS_NON_CHANGEABLE },
-{ .Port          = PORTS_D,
-  .Pins          = PORTS_PIN_1,
-  .Pull          = PORTS_PULLDOWN,
-  .OutputDrive   = PORTS_OPEN_COLECTOR,
-  .Speed         = PORTS_VERY_HIGH_SPEED,
-  .Mode          = PORTS_MODE_INPUT,
-  .Altern        = 0,
-  .Pin_direction = PORTS_NON_CHANGEABLE } };
+#include "Port_Cfg.h"
 
 /*this function is required by Ceedling to run any code before the test cases*/
 void setUp( void )
