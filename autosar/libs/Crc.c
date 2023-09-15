0d0152fb2125e0db6be1ce44c44d3c4617343689
FlavioCJM1 <flaviocjm@outlook.es>
2023-09-14 18:24:55 -0600

    It was added documentation of defines

--- autosar/libs/Crc.c
+++ autosar/libs/Crc.c
@@ -9,52 +9,90 @@
  * • CRC32
  * • CRC32P4: CRC32 0xF4ACFB13 polynomial
  * • CRC64: CRC-64-ECMA
- * 
- * This library was designed like a Runtime calculation: 
+ *
+ * This library was designed like a Runtime calculation:
  * Slower execution, but small code size (no ROM table)
  */
 
 #include "Std_Types.h"
 #include "Crc.h"
 
-#define CRC8_SAEJ1850_POLYNOMIAL          0x1D
-#define FIRSTCALLCRC8                     0xFF
-#define CRC8NBITS                         8
-#define CRC8MSB                           0x80
+/**
+  * @defgroup Values_CRC8_SAEJ1850 this defines are values for the calculation of CRC with 8 bits
+  @{ */
+#define CRC8_SAEJ1850_POLYNOMIAL          0x1D /*!< Polynomial of CRC8_SAEJ1850 */
+#define FIRSTCALLCRC8                     0xFF /*!< Value of CRC if is first call */
+#define CRC8NBITS                         8    /*!< Number of bits of a byte */
+#define CRC8MSB                           0x80 /*!< Most significant bit of a byte */
+/**
+  @} */
 
-#define CRC8H2F_POLYNOMIAL                0x2F
-#define FIRSTCALLCRC8H2F                  0xFF
-#define CRC8H2FNBITS                      8
-#define CRC8H2NMSB                        0x80
+/**
+  * @defgroup Values_CRC8H2F this defines are values for the calculation of CRC with 8 bits
+  @{ */
+#define CRC8H2F_POLYNOMIAL                0x2F /*!< Polynomial of CRC8H2F */
+#define FIRSTCALLCRC8H2F                  0xFF /*!< Value of CRC if is first call */
+#define CRC8H2FNBITS                      8    /*!< Number of bits of a byte */
+#define CRC8H2NMSB                        0x80 /*!< Most significant bit of a byte */
+/**
+  @} */
 
-#define CRC16_POLYNOMIAL                  0x1021
-#define FIRSTCALLCRC16                    0xFFFF
-#define CRC16NBITS                        8
-#define CRC16MSB                          0x8000
-#define CRC16_8LEFT                       8
+/**
+  * @defgroup Values_CRC16 this defines are values for the calculation of CRC with 16 bits
+  @{ */
+#define CRC16_POLYNOMIAL                  0x1021 /*!< Polynomial of CRC16 */
+#define FIRSTCALLCRC16                    0xFFFF /*!< Value of CRC if is first call */
+#define CRC16NBITS                        8      /*!< Number of bits of a byte */
+#define CRC16MSB                          0x8000 /*!< Most significant bit of 2 bytes */
+#define CRC16_8LEFT                       8      /*!< Number of shift bits on crc calculation */
+/**
+  @} */
 
-#define CRC_16BITARC_POLYNOMIAL_REFLECTED (uint16)0xA001
-#define CRC_16BITARC_LSB                  0x00001
+/**
+  * @defgroup Values_CRC16ARC this defines are values for the calculation of CRC with 16 bits and data reflected
+  @{ */
+#define CRC_16BITARC_POLYNOMIAL_REFLECTED (uint16)0xA001 /*!< Polynomial of CRC16ARC */
+#define CRC_16BITARC_LSB                  0x00001        /*!< Less significant bit */
+/**
+  @} */
 
-#define CRC_32BIT_POLYNOMIAL_REFLECTED    (uint32)0xEDB88320u
-#define CRC_32BIT_XORVALUE                0xFFFFFFFFu
-#define CRC_32BIT_LSB                     0x00000001u
+/**
+  * @defgroup Values_CRC32 this defines are values for the calculation of CRC with 16 bits and data reflected
+  @{ */
+#define CRC_32BIT_POLYNOMIAL_REFLECTED    (uint32)0xEDB88320u /*!< Polynomial of CRC32 */
+#define CRC_32BIT_XORVALUE                0xFFFFFFFFu         /*!< Value of CRC if is first call */
+#define CRC_32BIT_LSB                     0x00000001u         /*!< Less significant bit */
+/**
+  @} */
 
-#define CRC_CRC32P4_POLYNOMIAL_REFLECTED  (uint32)0xC8DF352Fu
-#define CRC_32BITP4_XORVALUE              0xFFFFFFFFu
-#define CRC_32BITP4_LSB                   0x00000001u
+/**
+  * @defgroup Values_CRC32P4 this defines are values for the calculation of CRC with 16 bits and data reflected
+  @{ */
+#define CRC_CRC32P4_POLYNOMIAL_REFLECTED  (uint32)0xC8DF352Fu /*!< Polynomial of CRC32P4 */
+#define CRC_32BITP4_XORVALUE              0xFFFFFFFFu         /*!< Value of CRC if is first call */
+#define CRC_32BITP4_LSB                   0x00000001u         /*!< Less significant bit */
+/**
+  @} */
 
-#define CRC_CRC64_POLYNOMIAL_REFLECTED    (uint64)0xC96C5795D7870F42UL
-#define CRC_64BIT_XORVALUE                0xFFFFFFFFFFFFFFFFUL
-#define CRC_64BIT_LSB                     0x0000000000000001UL
+/**
+  * @defgroup Values_CRC64 this defines are values for the calculation of CRC with 16 bits and data reflected
+  @{ */
+#define CRC_CRC64_POLYNOMIAL_REFLECTED    (uint64)0xC96C5795D7870F42UL /*!< Polynomial of CRC64 */
+#define CRC_64BIT_XORVALUE                0xFFFFFFFFFFFFFFFFUL         /*!< Value of CRC if is first call */
+#define CRC_64BIT_LSB                     0x0000000000000001UL         /*!< Less significant bit */
+/**
+  @} */
 
-#define REFLECTLSB                        0x01
-
-#define VENDOR_ID                         0x0000
-#define MODULE_ID                         0x0000
-#define CRC_SW_MAJOR_VERSION              0
-#define CRC_SW_MINOR_VERSION              0
-#define CRC_SW_PATCH_VERSION              0
+/**
+  * @defgroup CRC_VERSION_INFO this defines are values to know the version info about CRC
+  @{ */
+#define VENDOR_ID                         0x0000 /*!< CRC Vendor ID VALUE */
+#define MODULE_ID                         0x0000 /*!< CRC MODULE ID VALUE */
+#define CRC_SW_MAJOR_VERSION              0      /*!< CRC_SW_MAJOR_VERSION ID VALUE */
+#define CRC_SW_MINOR_VERSION              0      /*!< CRC_SW_MINOR_VERSION ID VALUE */
+#define CRC_SW_PATCH_VERSION              0      /*!< CRC_SW_PATCH_VERSION ID VALUE */
+/**
+  @} */
 
 /**
  * @brief   **Calculate a CRC of 8 bits**
@@ -128,7 +166,7 @@ uint8 Crc_CalculateCRC8( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 Crc_
  *
  * @param   Crc_Length Length of data block to be calculated in bytes.
  *
- * @param   Crc_StartValue8 Start value when the algorithm starts.
+ * @param   Crc_StartValue8H2F Start value when the algorithm starts.
  *
  * @param   Crc_IsFirstCall boolean variable to know if is the first time that we used the function.
  *
@@ -182,7 +220,7 @@ uint8 Crc_CalculateCRC8H2F( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint8 C
  *
  * @param   Crc_Length Length of data block to be calculated in bytes.
  *
- * @param   Crc_StartValue8 Start value when the algorithm starts.
+ * @param   Crc_StartValue16 Start value when the algorithm starts.
  *
  * @param   Crc_IsFirstCall boolean variable to know if is the first time that we used the function.
  *
@@ -240,7 +278,7 @@ uint16 Crc_CalculateCRC16( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint16 C
  *
  * @param   Crc_Length Length of data block to be calculated in bytes.
  *
- * @param   Crc_StartValue8 Start value when the algorithm starts.
+ * @param   Crc_StartValue16 Start value when the algorithm starts.
  *
  * @param   Crc_IsFirstCall boolean variable to know if is the first time that we used the function.
  *
@@ -298,7 +336,7 @@ uint16 Crc_CalculateCRC16ARC( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint1
  *
  * @param   Crc_Length Length of data block to be calculated in bytes.
  *
- * @param   Crc_StartValue8 Start value when the algorithm starts.
+ * @param   Crc_StartValue32 Start value when the algorithm starts.
  *
  * @param   Crc_IsFirstCall boolean variable to know if is the first time that we used the function.
  *
@@ -364,7 +402,7 @@ uint32 Crc_CalculateCRC32( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint32 C
  *
  * @param   Crc_Length Length of data block to be calculated in bytes.
  *
- * @param   Crc_StartValue8 Start value when the algorithm starts.
+ * @param   Crc_StartValue32 Start value when the algorithm starts.
  *
  * @param   Crc_IsFirstCall boolean variable to know if is the first time that we used the function.
  *
@@ -428,7 +466,7 @@ uint32 Crc_CalculateCRC32P4( const uint8 *Crc_DataPtr, uint32 Crc_Length, uint32
  *
  * @param   Crc_Length Length of data block to be calculated in bytes.
  *
- * @param   Crc_StartValue8 Start value when the algorithm starts.
+ * @param   Crc_StartValue64 Start value when the algorithm starts.
  *
  * @param   Crc_IsFirstCall boolean variable to know if is the first time that we used the function.
  *
