cf82aa18072258216871edd046ab69bd06cfa6fd
Enrique <joenorlu@gmail.com>
2023-09-16 19:34:24 -0600

    Changes

--- autosar/mcal/Dio.h
+++ autosar/mcal/Dio.h
@@ -19,76 +19,76 @@ typedef struct
 } Dio_ChannelGroupType;
 
 
-#define DIO_PIN_PA_00 (Dio_ChannelType)0x00u
-#define DIO_PIN_PA_01 (Dio_ChannelType)0x01u
-#define DIO_PIN_PA_02 (Dio_ChannelType)0x02u
-#define DIO_PIN_PA_03 (Dio_ChannelType)0x03u
-#define DIO_PIN_PA_04 (Dio_ChannelType)0x04u
-#define DIO_PIN_PA_05 (Dio_ChannelType)0x05u
-#define DIO_PIN_PA_06 (Dio_ChannelType)0x06u
-#define DIO_PIN_PA_07 (Dio_ChannelType)0x07u
-#define DIO_PIN_PA_08 (Dio_ChannelType)0x08u
-#define DIO_PIN_PA_09 (Dio_ChannelType)0x09u
-#define DIO_PIN_PA_10 (Dio_ChannelType)0x0Au
-#define DIO_PIN_PA_11 (Dio_ChannelType)0x0Bu
-#define DIO_PIN_PA_12 (Dio_ChannelType)0x0Cu
-#define DIO_PIN_PA_13 (Dio_ChannelType)0x0Du
-#define DIO_PIN_PA_14 (Dio_ChannelType)0x0Eu
-#define DIO_PIN_PA_15 (Dio_ChannelType)0x0Fu
+#define DIO_PIN_PA_00 (Dio_ChannelType)0x00u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_01 (Dio_ChannelType)0x01u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_02 (Dio_ChannelType)0x02u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_03 (Dio_ChannelType)0x03u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_04 (Dio_ChannelType)0x04u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_05 (Dio_ChannelType)0x05u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_06 (Dio_ChannelType)0x06u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_07 (Dio_ChannelType)0x07u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_08 (Dio_ChannelType)0x08u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_09 (Dio_ChannelType)0x09u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_10 (Dio_ChannelType)0x0Au /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_11 (Dio_ChannelType)0x0Bu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_12 (Dio_ChannelType)0x0Cu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_13 (Dio_ChannelType)0x0Du /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_14 (Dio_ChannelType)0x0Eu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PA_15 (Dio_ChannelType)0x0Fu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
 
 
-#define DIO_PIN_PB_00 (Dio_ChannelType)0x10u
-#define DIO_PIN_PB_01 (Dio_ChannelType)0x11u
-#define DIO_PIN_PB_02 (Dio_ChannelType)0x12u
-#define DIO_PIN_PB_03 (Dio_ChannelType)0x13u
-#define DIO_PIN_PB_04 (Dio_ChannelType)0x14u
-#define DIO_PIN_PB_05 (Dio_ChannelType)0x15u
-#define DIO_PIN_PB_06 (Dio_ChannelType)0x16u
-#define DIO_PIN_PB_07 (Dio_ChannelType)0x17u
-#define DIO_PIN_PB_08 (Dio_ChannelType)0x18u
-#define DIO_PIN_PB_09 (Dio_ChannelType)0x19u
-#define DIO_PIN_PB_10 (Dio_ChannelType)0x1Au
-#define DIO_PIN_PB_11 (Dio_ChannelType)0x1Bu
-#define DIO_PIN_PB_12 (Dio_ChannelType)0x1Cu
-#define DIO_PIN_PB_13 (Dio_ChannelType)0x1Du
-#define DIO_PIN_PB_14 (Dio_ChannelType)0x1Eu
-#define DIO_PIN_PB_15 (Dio_ChannelType)0x1Fu
+#define DIO_PIN_PB_00 (Dio_ChannelType)0x10u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_01 (Dio_ChannelType)0x11u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_02 (Dio_ChannelType)0x12u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_03 (Dio_ChannelType)0x13u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_04 (Dio_ChannelType)0x14u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_05 (Dio_ChannelType)0x15u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_06 (Dio_ChannelType)0x16u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_07 (Dio_ChannelType)0x17u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_08 (Dio_ChannelType)0x18u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_09 (Dio_ChannelType)0x19u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_10 (Dio_ChannelType)0x1Au /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_11 (Dio_ChannelType)0x1Bu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_12 (Dio_ChannelType)0x1Cu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_13 (Dio_ChannelType)0x1Du /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_14 (Dio_ChannelType)0x1Eu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PB_15 (Dio_ChannelType)0x1Fu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
 
 
-#define DIO_PIN_PC_00 (Dio_ChannelType)0x20u
-#define DIO_PIN_PC_01 (Dio_ChannelType)0x21u
-#define DIO_PIN_PC_02 (Dio_ChannelType)0x22u
-#define DIO_PIN_PC_03 (Dio_ChannelType)0x23u
-#define DIO_PIN_PC_04 (Dio_ChannelType)0x24u
-#define DIO_PIN_PC_05 (Dio_ChannelType)0x25u
-#define DIO_PIN_PC_06 (Dio_ChannelType)0x26u
-#define DIO_PIN_PC_07 (Dio_ChannelType)0x27u
-#define DIO_PIN_PC_08 (Dio_ChannelType)0x28u
-#define DIO_PIN_PC_09 (Dio_ChannelType)0x29u
-#define DIO_PIN_PC_10 (Dio_ChannelType)0x2Au
-#define DIO_PIN_PC_11 (Dio_ChannelType)0x2Bu
-#define DIO_PIN_PC_12 (Dio_ChannelType)0x2Cu
-#define DIO_PIN_PC_13 (Dio_ChannelType)0x2Du
-#define DIO_PIN_PC_14 (Dio_ChannelType)0x2Eu
-#define DIO_PIN_PC_15 (Dio_ChannelType)0x2Fu
+#define DIO_PIN_PC_00 (Dio_ChannelType)0x20u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_01 (Dio_ChannelType)0x21u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_02 (Dio_ChannelType)0x22u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_03 (Dio_ChannelType)0x23u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_04 (Dio_ChannelType)0x24u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_05 (Dio_ChannelType)0x25u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_06 (Dio_ChannelType)0x26u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_07 (Dio_ChannelType)0x27u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_08 (Dio_ChannelType)0x28u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_09 (Dio_ChannelType)0x29u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_10 (Dio_ChannelType)0x2Au /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_11 (Dio_ChannelType)0x2Bu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_12 (Dio_ChannelType)0x2Cu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_13 (Dio_ChannelType)0x2Du /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_14 (Dio_ChannelType)0x2Eu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PC_15 (Dio_ChannelType)0x2Fu /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
 
 
-#define DIO_PIN_PD_00 (Dio_ChannelType)0x30u
-#define DIO_PIN_PD_01 (Dio_ChannelType)0x31u
-#define DIO_PIN_PD_02 (Dio_ChannelType)0x32u
-#define DIO_PIN_PD_03 (Dio_ChannelType)0x33u
-#define DIO_PIN_PD_04 (Dio_ChannelType)0x34u
-#define DIO_PIN_PD_05 (Dio_ChannelType)0x35u
-#define DIO_PIN_PD_06 (Dio_ChannelType)0x36u
-#define DIO_PIN_PD_08 (Dio_ChannelType)0x38u
-#define DIO_PIN_PD_09 (Dio_ChannelType)0x39u
+#define DIO_PIN_PD_00 (Dio_ChannelType)0x30u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PD_01 (Dio_ChannelType)0x31u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PD_02 (Dio_ChannelType)0x32u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PD_03 (Dio_ChannelType)0x33u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PD_04 (Dio_ChannelType)0x34u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PD_05 (Dio_ChannelType)0x35u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PD_06 (Dio_ChannelType)0x36u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PD_08 (Dio_ChannelType)0x38u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PD_09 (Dio_ChannelType)0x39u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
 
 
-#define DIO_PIN_PF_00 (Dio_ChannelType)0x50u
-#define DIO_PIN_PF_01 (Dio_ChannelType)0x51u
-#define DIO_PIN_PF_02 (Dio_ChannelType)0x52u
-#define DIO_PIN_PF_03 (Dio_ChannelType)0x53u
-#define DIO_PIN_PF_04 (Dio_ChannelType)0x54u
+#define DIO_PIN_PF_00 (Dio_ChannelType)0x50u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PF_01 (Dio_ChannelType)0x51u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PF_02 (Dio_ChannelType)0x52u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PF_03 (Dio_ChannelType)0x53u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
+#define DIO_PIN_PF_04 (Dio_ChannelType)0x54u /* cppcheck-suppress misra-c2012-2.5 ; This define is used to indicate the pin value */
 
 
 Dio_LevelType Dio_ReadChannel( Dio_ChannelType ChannelId );
