4eb94182866c271cc6ffaf09951bbbe2f28b6609
Enrique <joenorlu@gmail.com>
2023-09-17 13:07:05 -0600

    Writing documentation for the Dio.c file

---
+++ autosar/mcal/Dio.c
@@ -0,0 +1,49 @@
+#include "Std_Types.h"
+#include "Dio.h"
+
+
+Dio_LevelType Dio_ReadChannel( Dio_ChannelType ChannelId )
+{
+
+}
+
+
+void Dio_WriteChannel( Dio_ChannelType ChannelId, Dio_LevelType Level )
+{
+
+}
+
+Dio_LevelType Dio_FlipChannel( Dio_ChannelType ChannelId )
+{
+
+}
+
+Dio_PortLevelType Dio_ReadPort( Dio_PortType PortId )
+{
+
+}
+
+void Dio_WritePort( Dio_PortType PortId, Dio_PortLevelType Level )
+{
+
+}
+
+Dio_PortLevelType Dio_ReadChannelGroup( const Dio_ChannelGroupType *ChannelGroupIdPtr )
+{
+
+}
+
+void Dio_WriteChannelGroup( const Dio_ChannelGroupType *ChannelGroupIdPtr, Dio_PortLevelType Level )
+{
+
+}
+
+Dio_PortLevelType Dio_GetVersionInfo( Std_VersionInfoType *versioninfo )
+{
+    
+}
+
+void Dio_MaskedWritePort( Dio_PortType PortId, Dio_PortLevelType Level, Dio_PortLevelType Mask )
+{
+    
+}
