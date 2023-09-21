4b77d42d28e5eb8292c11912e0443a0a02f095bd
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-09-21 16:26:30 -0600

    GPT-Code-Implementation. Rebase made.

---
+++ autosar/mcal/Gpt.c
@@ -0,0 +1,71 @@
+#include "Gpt.h"
+
+void Gpt_GetVersionInfo( Std_VersionInfoType *VersionInfoPtr )
+{
+
+}
+
+void Gpt_Init( const Gpt_ConfigType *ConfigPtr )
+{
+
+}
+
+void Gpt_DeInit( void )
+{
+
+}
+
+Gpt_ValueType Gpt_GetTimeElapsed(Gpt_ChannelType Channel )
+{
+
+}
+
+Gpt_ValueType Gpt_GetTimeRemaining( Gpt_ChannelType Channel )
+{
+
+}
+
+void Gpt_StartTimer( Gpt_ChannelType Channel, Gpt_ValueType Value )
+{
+
+}
+
+void Gpt_StopTimer( Gpt_ChannelType Channel )
+{
+
+}
+
+void Gpt_EnableNotification( Gpt_ChannelType Channel )
+{
+
+}
+
+void Gpt_DisableNotification( Gpt_ChannelType Channel )
+{
+
+}
+
+void Gpt_SetMode( Gpt_ModeType Mode )
+{
+
+}
+
+void Gpt_DisableWakeUp( Gpt_ChannelType Channel )
+{
+
+}
+
+void Gpt_EnableWakeUp( Gpt_ChannelType Channel )
+{
+
+}
+
+void Gpt_Notification_TIM6( void )
+{
+
+}
+
+void Gpt_Notification_TIM7( void )
+{
+    
+}
--- autosar/mcal/regs/Registers.h
+++ autosar/mcal/regs/Registers.h
@@ -8,8 +8,166 @@
 #define REGISTERS_H
 
 /**
+<<<<<<< HEAD
  * @brief   Nested Vectored Interrupt Controller (NVIC) interrupt types for Cortex-M0+ and STM32G0xxxx.
  *          This enumeration lists the Cortex-M0+ exceptions and STM32G0xxxx microcontroller interrupts
+=======
+ * @name    Base address values
+ * Symbols to specify the Base values of the registers.
+ */
+/**@{*/
+#define PERIPH_BASE        0x40000000UL                       /*!< Peripherals register base address*/
+#define FLASH_BASE_ADDRESS 0x08000000                         /*!< FLASH ADDRESS */
+#define SRAM_BASE_ADDRESS  0x20000000                         /*!< SRAM ADDRESS */
+#define APB1_BASE_ADDRESS  ( PERIPH_BASE + 0x00000000 )       /*!< APB1 ADDRESS */
+#define APB2_BASE_ADDRESS  ( PERIPH_BASE + 0x00010000 )       /*!< APB2 ADDRESS */
+#define AHB_BASE_ADDRESS   ( PERIPH_BASE + 0x00020000 )       /*!< AHB ADDRESS */
+#define RCC_BASE_ADDRESS   0x40021000                         /*!< RCC ADDRESS */
+#define IOPORT_BASE        0x50000000                         /*!< IOPORT ADDRESS */
+#define PORTA_BASE         ( IOPORT_BASE + 0x00000000UL )     /*!< GPIOA ADDRESS */
+#define PORTB_BASE         ( IOPORT_BASE + 0x00000400UL )     /*!< GPIOB ADDRESS */
+#define PORTC_BASE         ( IOPORT_BASE + 0x00000800UL )     /*!< GPIOC ADDRESS */
+#define PORTD_BASE         ( IOPORT_BASE + 0x00000C00UL )     /*!< GPIOD ADDRESS */
+#define PORTE_BASE         ( IOPORT_BASE + 0x00001000UL )     /*!< GPIOE ADDRESS */
+#define PORTF_BASE         ( IOPORT_BASE + 0x00001400UL )     /*!< GPIOF ADDRESS */
+#define SCS_BASE           ( 0xE000E000UL )                   /*!< System Control Space Base Address */
+#define NVIC_BASE          ( SCS_BASE + 0x0100UL )            /*!< NVIC Base Address */
+#define NVIC               ( (Nvic_RegisterType *)NVIC_BASE ) /*!< NVIC configuration struct */
+#define TIM6_BASE_ADDRESS  0x40001000                         /*!< TIM6 register base address */
+#define TIM7_BASE_ADDRESS  0x40001400                         /*!< TIM7 register base address */
+/**@}*/
+
+/**
+ * @brief RCC registers struct.
+ */
+typedef struct
+{
+    volatile uint32 CR;        /*!< RCC clock control register*/
+    volatile uint32 ICSCR;     /*!< RCC internal clock sources calibration register*/
+    volatile uint32 CFGR;      /*!< Clock configuration register*/
+    volatile uint32 PLLCFGR;   /*!< PLL configuration register*/
+    volatile uint32 Reserv;    /*!< Reserv memory space*/
+    volatile uint32 CRRCR;     /*!< clock recovery RC register*/
+    volatile uint32 CIER;      /*!< Clock interrupt enable register*/
+    volatile uint32 CIFR;      /*!< Clock interrupt flag register*/
+    volatile uint32 CICR;      /*!< Clock interrupt clear register*/
+    volatile uint32 IOPRSTR;   /*!< I/O port reset register*/
+    volatile uint32 AHBRSTR;   /*!< AHB peripheral reset register*/
+    volatile uint32 APBRSTR1;  /*!< APB1 peripheral reset register*/
+    volatile uint32 APBRSTR2;  /*!< APB2 peripheral reset register*/
+    volatile uint32 IOPENR;    /*!< I/O port clock enable register*/
+    volatile uint32 AHBENR;    /*!< AHB peripheral clock enable register*/
+    volatile uint32 APBENR1;   /*!< APB1 peripheral clock enable register */
+    volatile uint32 APBENR2;   /*!< APB2 peripheral clock enable register */
+    volatile uint32 IOPSMENR;  /*!< I/O port in Sleep mode clock enable register*/
+    volatile uint32 AHBSMENR;  /*!< AHB peripheral clock enable in sleep/stop mode register*/
+    volatile uint32 APBSMENR1; /*!< APB1 peripheral clock enable in Sleep/stop mode register */
+    volatile uint32 APBSMENR2; /*!< APB2 peripheral clock enable in Sleep/stop mode register*/
+    volatile uint32 CCIPR;     /*!< Peripherals independent clock configuration register*/
+    volatile uint32 CCIPR2;    /*!< Peripherals independent clock configuration register 2*/
+    volatile uint32 BDCR;      /*!< RTC domain control register*/
+    volatile uint32 CSR;       /*!< control/status register*/
+} Rcc_RegisterType;
+
+/**
+ * @brief GPIOS registers struct.
+ */
+typedef struct
+{
+    volatile uint32 MODER;   /*!< GPIO port mode register*/
+    volatile uint32 OTYPER;  /*!< GPIO port output type register*/
+    volatile uint32 OSPEEDR; /*!< GPIO port output speed register*/
+    volatile uint32 PUPDR;   /*!< GPIO port pull-up/pull-down register*/
+    volatile uint32 IDR;     /*!< GPIO port input data register*/
+    volatile uint32 ODR;     /*!< GPIO port output data register*/
+    volatile uint32 BSRR;    /*!< GPIO port bit set/reset register*/
+    volatile uint32 LCKR;    /*!< GPIO port configuration lock register*/
+    volatile uint32 AFRL;    /*!< GPIO alternate function low register*/
+    volatile uint32 AFRH;    /*!< GPIO alternate function high register*/
+    volatile uint32 BRR;     /*!< GPIO port bit reset register*/
+} Port_RegisterType;
+
+
+/**
+ * @brief GPIOS registers struct for Dio.
+ */
+typedef struct
+{
+    volatile uint32 Reserved1[ 4 ]; /*!< Reserved register*/
+    volatile uint32 IDR;            /*!< GPIO port input data register*/
+    volatile uint32 ODR;            /*!< GPIO port output data register*/
+    volatile uint32 BSRR;           /*!< GPIO port bit set/reset register*/
+    volatile uint32 Reserved2[ 3 ]; /*!< Reserved register*/
+    volatile uint32 BRR;            /*!< GPIO port bit reset register*/
+} Dio_RegisterType;
+
+/**
+ * @brief GPT registers struct.
+ */
+typedef struct
+{
+  volatile uint32 CR1;        /*!< TIM control register 1 */
+  volatile uint32 CR2;        /*!< TIM control register 2 */
+  volatile uint32 Reserved0;  /*!< Reserved memory space */
+  volatile uint32 DIER;       /*!< TIM DMA/Interrupt enable register */
+  volatile uint32 SR;         /*!< TIM status register */
+  volatile uint32 EGR;        /*!< TIM event generation register */
+  volatile uint32 Reserved1;  /*!< Reserved memory space */
+  volatile uint16 Reserved2;  /*!< Reserved memory space */
+  volatile uint32 CNT;        /*!< TIM counter */
+  volatile uint32 PSC;        /*!< TIM prescaler */
+  volatile uint32 ARR;        /*!< TIM auto-reload register */
+} Gpt_RegisterType;
+
+/**
+  * @defgroup  Struct casting to base address of the PORTS and RCC
+  @{ */
+#define PORTA                ( (Port_RegisterType *)PORTA_BASE )      /*!< Access to PORTA registers*/
+#define PORTB                ( (Port_RegisterType *)PORTB_BASE )      /*!< Access to PORTB registers*/
+#define PORTC                ( (Port_RegisterType *)PORTC_BASE )      /*!< Access to PORTC registers*/
+#define PORTD                ( (Port_RegisterType *)PORTD_BASE )      /*!< Access to PORTD registers*/
+#define PORTE                ( (Port_RegisterType *)PORTE_BASE )      /*!< Access to PORTE registers*/
+#define PORTF                ( (Port_RegisterType *)PORTF_BASE )      /*!< Access to PORTF registers*/
+#define RCC                  ( (RCC_RegisterType *)RCC_BASE_ADDRESS ) /*!< Access to RCC registers*/
+/**@}*/
+
+/**
+  * @defgroup  Struct-Dio casting to base address of the PORTS and RCC for Dio
+  @{ */
+#define DIOA                 ( (Dio_RegisterType *)PORTA_BASE ) /*!< Access to DIOA registers*/
+#define DIOB                 ( (Dio_RegisterType *)PORTB_BASE ) /*!< Access to DIOB registers*/
+#define DIOC                 ( (Dio_RegisterType *)PORTC_BASE ) /*!< Access to DIOC registers*/
+#define DIOD                 ( (Dio_RegisterType *)PORTD_BASE ) /*!< Access to DIOD registers*/
+#define DIOE                 ( (Dio_RegisterType *)PORTE_BASE ) /*!< Access to DIOE registers*/
+#define DIOF                 ( (Dio_RegisterType *)PORTF_BASE ) /*!< Access to DIOF registers*/
+/**@}*/
+
+/**
+  * @defgroup Clock-eneable of different ports
+  @{ */
+#define RCC_GPIOA_CLK_EN( )  RCC->IOPENR |= 0x01u; /*!< GPIO PORTA clock enable*/
+#define RCC_GPIOB_CLK_EN( )  RCC->IOPENR |= 0x02u; /*!< GPIO PORTB clock enable*/
+#define RCC_GPIOC_CLK_EN( )  RCC->IOPENR |= 0x04u; /*!< GPIO PORTC clock enable*/
+#define RCC_GPIOD_CLK_EN( )  RCC->IOPENR |= 0x08u; /*!< GPIO PORTD clock enable*/
+#define RCC_GPIOE_CLK_EN( )  RCC->IOPENR |= 0x10u; /*!< GPIO PORTE clock enable*/
+#define RCC_GPIOF_CLK_EN( )  RCC->IOPENR |= 0x20u; /*!< GPIO PORTF clock enable*/
+/**@}*/
+
+/**
+  * @defgroup Clock-disable of different ports
+  @{ */
+#define RCC_GPIOA_CLK_DIS( ) RCC->IOPENR &= ~( 1u << 0u ); /*!< GPIO PORTA clock disable*/
+#define RCC_GPIOB_CLK_DIS( ) RCC->IOPENR &= ~( 1u << 1u ); /*!< GPIO PORTB clock disable*/
+#define RCC_GPIOC_CLK_DIS( ) RCC->IOPENR &= ~( 1u << 2u ); /*!< GPIO PORTC clock disable*/
+#define RCC_GPIOD_CLK_DIS( ) RCC->IOPENR &= ~( 1u << 3u ); /*!< GPIO PORTD clock disable*/
+#define RCC_GPIOE_CLK_DIS( ) RCC->IOPENR &= ~( 1u << 4u ); /*!< GPIO PORTE clock disable*/
+#define RCC_GPIOF_CLK_DIS( ) RCC->IOPENR &= ~( 1u << 5u ); /*!< GPIO PORTF clock disable*/
+/**@}*/
+
+/**
+ * @brief Nested Vectored Interrupt Controller (NVIC) interrupt types for Cortex-M0+ and STM32G0xxxx.
+ *  This enumeration lists the Cortex-M0+ exceptions and STM32G0xxxx microcontroller interrupts
+>>>>>>> 70a249d (GPT-Code-Implementation. Rebase made.)
  */
 typedef enum
 {
