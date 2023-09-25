3043e5caf6bbd7c74f39b3f7315aafffdf7b8ede
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-09-25 14:52:49 -0600

    GPT-Code-Implementation. Creation of the functions Gpt_Init, GetTimeElapsed & GetTimeRemaining.

--- autosar/mcal/Gpt.c
+++ autosar/mcal/Gpt.c
@@ -1,13 +1,35 @@
+#include "Bfx.h"
+#include "Std_Types.h"
+#include "Registers.h"
 #include "Gpt.h"
 
-void Gpt_GetVersionInfo( Std_VersionInfoType *VersionInfoPtr )
-{
-
-}
+static const Gpt_ConfigType *LocalGptPtr = NULL_PTR;
 
 void Gpt_Init( const Gpt_ConfigType *ConfigPtr )
 {
+    switch ( ConfigPtr->Channel )
+    {
+        case Channel1:
+            Bfx_SetBit_u32u8( *(const unsigned long *)0x4002103C, 4 ); /*Activation TIM6EN: bit in RCC_APBENR1*/
+            Bfx_SetBitMask_u32u32( TIM6.PSC, ConfigPtr->Prescaler );
+            Bfx_SetBits_u32u8u8u8( TIM6.ARR, 0, 16u, 0 ); /*Clearing the reset value of TIMx_ARR*/
+            Bfx_SetBitMask_u32u32( TIM6.ARR, ConfigPtr->Period ); /*Writing the value of Period on TIMx_ARR*/
+            Bfx_SetBit_u32u8( TIM6.SR, 0 ); /*Clearing the update interrupt flag of TIMx_SR*/
+            break;
 
+        case Channel2:
+            Bfx_SetBit_u32u8( *(const unsigned long *)0x4002103C, 5 ); /*Activation TIM7EN: bit in RCC_APBENR1*/
+            Bfx_SetBitMask_u32u32( TIM7.PSC, ConfigPtr->Prescaler ); /*Writing the value of the prescaler on TIMx_PSC*/
+            Bfx_SetBits_u32u8u8u8( TIM7.ARR, 0, 16u, 0 ); /*Clearing the reset value of TIMx_ARR*/
+            Bfx_SetBitMask_u32u32( TIM7.ARR, ConfigPtr->Period ); /*Writing the value of Period on TIMx_ARR*/
+            Bfx_SetBit_u32u8( TIM7.SR, 0 ); /*Clearing the update interrupt flag of TIMx_SR*/
+            break;
+        
+        default:
+            break;
+    }
+    
+    LocalGptPtr = ConfigPtr;
 }
 
 void Gpt_DeInit( void )
@@ -17,12 +39,36 @@ void Gpt_DeInit( void )
 
 Gpt_ValueType Gpt_GetTimeElapsed(Gpt_ChannelType Channel )
 {
-
+    switch ( Channel )
+    {
+        case Channel1:
+            return TIM6->CNT;
+            break;
+        
+        case Channel2:
+            return TIM7->CNT;
+        break;
+        
+        default:
+            break;
+    }
 }
 
 Gpt_ValueType Gpt_GetTimeRemaining( Gpt_ChannelType Channel )
 {
-
+    switch ( Channel )
+    {
+        case Channel1:
+            return TIM6->ARR - TIM6->CNT;
+            break;
+        
+        case Channel2:
+            return TIM7->ARR - TIM7->CNT;
+        break;
+        
+        default:
+            break;
+    }
 }
 
 void Gpt_StartTimer( Gpt_ChannelType Channel, Gpt_ValueType Value )
@@ -45,21 +91,6 @@ void Gpt_DisableNotification( Gpt_ChannelType Channel )
 
 }
 
-void Gpt_SetMode( Gpt_ModeType Mode )
-{
-
-}
-
-void Gpt_DisableWakeUp( Gpt_ChannelType Channel )
-{
-
-}
-
-void Gpt_EnableWakeUp( Gpt_ChannelType Channel )
-{
-
-}
-
 void Gpt_Notification_TIM6( void )
 {
 
@@ -67,5 +98,5 @@ void Gpt_Notification_TIM6( void )
 
 void Gpt_Notification_TIM7( void )
 {
-    
+
 }
