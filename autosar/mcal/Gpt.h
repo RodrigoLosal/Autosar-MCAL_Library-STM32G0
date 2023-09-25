9adabd7819f95b75f5bf4c236de7338bbe04bc05
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-09-25 14:52:49 -0600

    GPT-Code-Implementation. Creation of the functions Gpt_Init, GetTimeElapsed & GetTimeRemaining.

--- autosar/mcal/Gpt.h
+++ autosar/mcal/Gpt.h
@@ -1,27 +1,21 @@
-#include "Registers.h"
-#include "Std_Types.h"
-#include "Bfx.h"
+#ifndef GPT_H__
+#define GPT_H__
 
 typedef struct _Gpt_ConfigType
 {
-    
+    uint8 Channel;
+    uint16 Prescaler;
+    uint16 Period;
 } Gpt_ConfigType;
 
 typedef enum
 {
-	TIM6 = 0,
-	TIM7,
+	Channel1,
+	Channel2,
 } Gpt_ChannelType;
 
 typedef uint32 Gpt_ValueType;
 
-typedef enum
-{
-	GPT_MODE_NORMAL = 0x00,
-	GPT_MODE_SLEEP = 0x01
-} Gpt_ModeType;
-
-void Gpt_GetVersionInfo( Std_VersionInfoType *VersionInfoPtr );
 void Gpt_Init( const Gpt_ConfigType *ConfigPtr );
 void Gpt_DeInit( void );
 Gpt_ValueType Gpt_GetTimeElapsed( Gpt_ChannelType Channel );
@@ -30,8 +24,7 @@ void Gpt_StartTimer( Gpt_ChannelType Channel, Gpt_ValueType Value );
 void Gpt_StopTimer( Gpt_ChannelType Channel );
 void Gpt_EnableNotification( Gpt_ChannelType Channel );
 void Gpt_DisableNotification( Gpt_ChannelType Channel );
-void Gpt_SetMode( Gpt_ModeType Mode );
-void Gpt_DisableWakeUp( Gpt_ChannelType Channel );
-void Gpt_EnableWakeUp( Gpt_ChannelType Channel );
 void Gpt_Notification_TIM6( void );
 void Gpt_Notification_TIM7( void );
+
+#endif
--- autosar/mcal/regs/Registers.h
+++ autosar/mcal/regs/Registers.h
@@ -102,17 +102,16 @@ typedef struct
  */
 typedef struct
 {
-  volatile uint32 CR1;        /*!< TIM control register 1 */
-  volatile uint32 CR2;        /*!< TIM control register 2 */
-  volatile uint32 Reserved0;  /*!< Reserved memory space */
-  volatile uint32 DIER;       /*!< TIM DMA/Interrupt enable register */
-  volatile uint32 SR;         /*!< TIM status register */
-  volatile uint32 EGR;        /*!< TIM event generation register */
-  volatile uint32 Reserved1;  /*!< Reserved memory space */
-  volatile uint16 Reserved2;  /*!< Reserved memory space */
-  volatile uint32 CNT;        /*!< TIM counter */
-  volatile uint32 PSC;        /*!< TIM prescaler */
-  volatile uint32 ARR;        /*!< TIM auto-reload register */
+  volatile uint32 CR1;          /*!< TIM control register 1 */
+  volatile uint32 CR2;          /*!< TIM control register 2 */
+  volatile uint32 Reserved0;    /*!< Reserved memory space */
+  volatile uint32 DIER;         /*!< TIM DMA/Interrupt enable register */
+  volatile uint32 SR;           /*!< TIM status register */
+  volatile uint32 EGR;          /*!< TIM event generation register */
+  volatile uint32 Reserved1[3]; /*!< Reserved memory space */
+  volatile uint32 CNT;          /*!< TIM counter */
+  volatile uint32 PSC;          /*!< TIM prescaler */
+  volatile uint32 ARR;          /*!< TIM auto-reload register */
 } Gpt_RegisterType;
 
 /**
