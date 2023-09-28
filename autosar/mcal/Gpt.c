2f6e1b45091383ac5a7af98a3f8e79f02fbeff66
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-09-28 03:25:43 -0600

    GPT-Code-Implementation. Corrections made on the functions.

--- autosar/mcal/Gpt.c
+++ autosar/mcal/Gpt.c
@@ -6,26 +6,15 @@
 
 void Gpt_Init( const Gpt_ConfigType *ConfigPtr )
 {
-    switch( ConfigPtr->Channel )
+    Gpt_RegisterType *channel;
+    Gpt_RegisterType *channels[ GPT_NUMBER_OF_CHANNELS ] = { TIM6, TIM7 };
+
+    for( uint8 j = 0; j < GPT_NUMBER_OF_CHANNELS; j++ )
     {
-        case Channel1:
-            /*cppcheck-suppress misra-c2012-11.4 ; Required for the unavailability of the use of RCC register.*/
-            Bfx_SetBit_u32u8( (uint32 *)*(volatile unsigned long *)0x4002103C, 4 ); /*Activation TIM6EN: bit in RCC_APBENR1*/
-            Bfx_SetBitMask_u32u32( (uint32 *)&TIM6->PSC, ConfigPtr->Prescaler );    /*Writing the value of the prescaler on TIMx_PSC*/
-            Bfx_PutBit_u32u8u8( (uint32 *)&TIM6->CR1, 3u, ConfigPtr->ChannelMode ); /*Writing the OPM: bit of TIMx_CR1 for continuous or one-pulse mode*/
-            Bfx_ClrBit_u32u8( (uint32 *)&TIM6->SR, 0 );                             /*Clearing the update interrupt flag of TIMx_SR*/
-            break;
-
-        case Channel2:
-            /*cppcheck-suppress misra-c2012-11.4 ; Required for the unavailability of the use of RCC register.*/
-            Bfx_SetBit_u32u8( (uint32 *)*(volatile unsigned long *)0x4002103C, 5 ); /*Activation TIM7EN: bit in RCC_APBENR1*/
-            Bfx_SetBitMask_u32u32( (uint32 *)&TIM7->PSC, ConfigPtr->Prescaler );    /*Writing the value of the prescaler on TIMx_PSC*/
-            Bfx_PutBit_u32u8u8( (uint32 *)&TIM7->CR1, 3u, ConfigPtr->ChannelMode ); /*Writing the OPM: bit of TIMx_CR1 for continuous or one-pulse mode*/
-            Bfx_ClrBit_u32u8( (uint32 *)&TIM7->SR, 0 );                             /*Clearing the update interrupt flag of TIMx_SR*/
-            break;
-
-        default:
-            break;
+        channel = channels[ ( &ConfigPtr[ j ] )->Channel ];
+        Bfx_SetBitMask_u32u32( (uint32 *)&channel->PSC, (uint32)( &ConfigPtr[ j ] )->Prescaler );    /*Writing the value of the prescaler on TIMx_PSC*/
+        Bfx_PutBit_u32u8u8( (uint32 *)&channel->CR1, 3u, (uint32)( &ConfigPtr[ j ] )->ChannelMode ); /*Writing the OPM: bit of TIMx_CR1 for continuous or one-pulse mode*/
+        Bfx_ClrBit_u32u8( (uint32 *)&channel->SR, 0 );                                               /*Clearing the update interrupt flag of TIMx_SR*/
     }
 <<<<<<< HEAD
 
@@ -48,21 +37,16 @@ void Gpt_Init( const Gpt_ConfigType *ConfigPtr )
 
 void Gpt_DeInit( void )
 {
-    /*TIM6 DeInit*/
+    Gpt_RegisterType *channel;
+    Gpt_RegisterType *channels[ 2 ] = { TIM6, TIM7 };
 
-    /*cppcheck-suppress misra-c2012-11.4 ; Required for the unavailability of the use of RCC register.*/
-    Bfx_ClrBit_u32u8( (uint32 *)*(volatile unsigned long *)0x4002103C, 4 ); /*Clearing TIM6EN: bit in RCC_APBENR1*/
-    Bfx_SetBits_u32u8u8u8( (uint32 *)&TIM6->PSC, 0, 16u, 0 );               /*Clearing the value of the prescaler on TIMx_PSC*/
-    Bfx_ClrBit_u32u8( (uint32 *)&TIM6->SR, 0 );                             /*Clearing the update interrupt flag of TIMx_SR*/
-    Bfx_SetBits_u32u8u8u8( (uint32 *)&TIM6->ARR, 0, 16u, 1 );               /*Setting back the reset value of TIMx_ARR*/
-
-    /*TIM7 DeInit*/
-
-    /*cppcheck-suppress misra-c2012-11.4 ; Required for the unavailability of the use of RCC register.*/
-    Bfx_ClrBit_u32u8( (uint32 *)*(volatile unsigned long *)0x4002103C, 5 ); /*Clearing TIM7EN: bit in RCC_APBENR1*/
-    Bfx_SetBits_u32u8u8u8( (uint32 *)&TIM7->PSC, 0, 16u, 0 );               /*Clearing the value of the prescaler on TIMx_PSC*/
-    Bfx_ClrBit_u32u8( (uint32 *)&TIM7->SR, 0 );                             /*Clearing the update interrupt flag of TIMx_SR*/
-    Bfx_SetBits_u32u8u8u8( (uint32 *)&TIM7->ARR, 0, 16u, 1 );               /*Setting back the reset value of TIMx_ARR*/
+    for( uint8 j = 0; j < GPT_NUMBER_OF_CHANNELS; j++ )
+    {
+        channel = channels[ j ];
+        Bfx_SetBits_u32u8u8u8( (uint32 *)&channel->PSC, 0, 16u, 0 ); /*Clearing the value of the prescaler on TIMx_PSC*/
+        Bfx_ClrBit_u32u8( (uint32 *)&channel->SR, 0 );               /*Clearing the update interrupt flag of TIMx_SR*/
+        Bfx_SetBits_u32u8u8u8( (uint32 *)&channel->ARR, 0, 16u, 1 ); /*Setting back the reset value of TIMx_ARR*/
+    }
 }
 
 Gpt_ValueType Gpt_GetTimeElapsed( Gpt_ChannelType Channel )
@@ -72,7 +56,10 @@ Gpt_ValueType Gpt_GetTimeElapsed( Gpt_ChannelType Channel )
     switch ( Channel )
 =======
     Gpt_ValueType TimeElapsed;
+    Gpt_RegisterType *channel;
+    Gpt_RegisterType *channels[ GPT_NUMBER_OF_CHANNELS ] = { TIM6, TIM7 };
 
+<<<<<<< HEAD
     switch( Channel )
 >>>>>>> 8a205fd (GPT-Code-Implementation. Autoformat runned, fixed some warnings.)
     {
@@ -92,6 +79,10 @@ Gpt_ValueType Gpt_GetTimeElapsed( Gpt_ChannelType Channel )
 
 >>>>>>> cf843bd (GPT-Code-Implementation. Creation of the files, control variables & functions. TIM6 & TIM7 registers added to Registers.h)
 =======
+=======
+    channel     = channels[ Channel ];
+    TimeElapsed = ( 1 / ( 16000000 / channel->PSC ) ) * ( channel->CNT ); /*Convertion to ms*/
+>>>>>>> d5b5122 (GPT-Code-Implementation. Corrections made on the functions.)
 
     return TimeElapsed;
 >>>>>>> 8a205fd (GPT-Code-Implementation. Autoformat runned, fixed some warnings.)
@@ -104,7 +95,10 @@ Gpt_ValueType Gpt_GetTimeRemaining( Gpt_ChannelType Channel )
     switch ( Channel )
 =======
     Gpt_ValueType TimeRemaining;
+    Gpt_RegisterType *channel;
+    Gpt_RegisterType *channels[ GPT_NUMBER_OF_CHANNELS ] = { TIM6, TIM7 };
 
+<<<<<<< HEAD
     switch( Channel )
 >>>>>>> 8a205fd (GPT-Code-Implementation. Autoformat runned, fixed some warnings.)
     {
@@ -124,6 +118,11 @@ Gpt_ValueType Gpt_GetTimeRemaining( Gpt_ChannelType Channel )
 
 >>>>>>> cf843bd (GPT-Code-Implementation. Creation of the files, control variables & functions. TIM6 & TIM7 registers added to Registers.h)
 =======
+=======
+    channel       = channels[ Channel ];
+    TimeRemaining = ( 1 / ( 16000000 / channel->PSC ) ) * ( channel->ARR - channel->CNT ); /*Convertion to ms*/
+    ;
+>>>>>>> d5b5122 (GPT-Code-Implementation. Corrections made on the functions.)
 
     return TimeRemaining;
 >>>>>>> 8a205fd (GPT-Code-Implementation. Autoformat runned, fixed some warnings.)
@@ -131,76 +130,43 @@ Gpt_ValueType Gpt_GetTimeRemaining( Gpt_ChannelType Channel )
 
 void Gpt_StartTimer( Gpt_ChannelType Channel, Gpt_ValueType Value )
 {
-    switch( Channel )
-    {
-        case Channel1:
-            Bfx_SetBits_u32u8u8u8( (uint32 *)&TIM6->ARR, 0, 16u, 0 ); /*Clearing the reset value of TIMx_ARR*/
-            Bfx_SetBitMask_u32u32( (uint32 *)&TIM6->ARR, Value );     /*Writing the value of Period on TIMx_ARR*/
-            Bfx_SetBit_u32u8( (uint32 *)&TIM6->CR1, 0 );              /*Setting the CEN: bit of TIMx_CR1*/
-            break;
+    Gpt_RegisterType *channel;
+    Gpt_RegisterType *channels[ GPT_NUMBER_OF_CHANNELS ] = { TIM6, TIM7 };
 
-        case Channel2:
-            Bfx_SetBits_u32u8u8u8( (uint32 *)&TIM7->ARR, 0, 16u, 0 ); /*Clearing the reset value of TIMx_ARR*/
-            Bfx_SetBitMask_u32u32( (uint32 *)&TIM7->ARR, Value );     /*Writing the value of Period on TIMx_ARR*/
-            Bfx_SetBit_u32u8( (uint32 *)&TIM6->CR1, 0 );              /*Setting the CEN: bit of TIMx_CR1*/
-            break;
-
-        default:
-            break;
-    }
+    channel = channels[ Channel ];
+    Bfx_SetBits_u32u8u8u8( (uint32 *)&channel->ARR, 0, 16u, 0 ); /*Clearing the reset value of TIMx_ARR*/
+    Bfx_SetBitMask_u32u32( (uint32 *)&channel->ARR, Value );     /*Writing the value of Period on TIMx_ARR*/
+    Bfx_SetBit_u32u8( (uint32 *)&channel->CR1, 0 );              /*Setting the CEN: bit of TIMx_CR1*/
 }
 
 void Gpt_StopTimer( Gpt_ChannelType Channel )
 {
-    switch( Channel )
-    {
-        case Channel1:
-            Bfx_ClrBit_u32u8( (uint32 *)&TIM6->CR1, 0 ); /*Clearing the CEN: bit of TIMx_CR1*/
-            break;
+    Gpt_RegisterType *channel;
+    Gpt_RegisterType *channels[ GPT_NUMBER_OF_CHANNELS ] = { TIM6, TIM7 };
 
-        case Channel2:
-            Bfx_ClrBit_u32u8( (uint32 *)&TIM6->CR1, 0 ); /*Clearing the CEN: bit of TIMx_CR1*/
-            break;
-
-        default:
-            break;
-    }
+    channel = channels[ Channel ];
+    Bfx_ClrBit_u32u8( (uint32 *)&channel->CR1, 0 ); /*Clearing the CEN: bit of TIMx_CR1*/
 }
 
 void Gpt_EnableNotification( Gpt_ChannelType Channel )
 {
-    switch( Channel )
-    {
-        case Channel1:
-            Bfx_ClrBit_u32u8( (uint32 *)&TIM6->CR1, 1 ); /*Clearing the UDIS: bit of TIMx_CR1*/
-            break;
+    Gpt_RegisterType *channel;
+    Gpt_RegisterType *channels[ GPT_NUMBER_OF_CHANNELS ] = { TIM6, TIM7 };
 
-        case Channel2:
-            Bfx_ClrBit_u32u8( (uint32 *)&TIM6->CR1, 1 ); /*Clearing the UDIS: bit of TIMx_CR1*/
-            break;
-
-        default:
-            break;
-    }
+    channel = channels[ Channel ];
+    Bfx_ClrBit_u32u8( (uint32 *)&channel->CR1, 1 ); /*Clearing the UDIS: bit of TIMx_CR1*/
 }
 
 void Gpt_DisableNotification( Gpt_ChannelType Channel )
 {
-    switch( Channel )
-    {
-        case Channel1:
-            Bfx_SetBit_u32u8( (uint32 *)&TIM6->CR1, 1 ); /*Setting the UDIS: bit of TIMx_CR1*/
-            break;
+    Gpt_RegisterType *channel;
+    Gpt_RegisterType *channels[ GPT_NUMBER_OF_CHANNELS ] = { TIM6, TIM7 };
 
-        case Channel2:
-            Bfx_SetBit_u32u8( (uint32 *)&TIM6->CR1, 1 ); /*Setting the UDIS: bit of TIMx_CR1*/
-            break;
-
-        default:
-            break;
-    }
+    channel = channels[ Channel ];
+    Bfx_SetBit_u32u8( (uint32 *)&channel->CR1, 1 ); /*Setting the UDIS: bit of TIMx_CR1*/
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 void Gpt_SetMode( Gpt_ModeType Mode )
@@ -220,11 +186,16 @@ void Gpt_EnableWakeUp( Gpt_ChannelType Channel )
 
 >>>>>>> cf843bd (GPT-Code-Implementation. Creation of the files, control variables & functions. TIM6 & TIM7 registers added to Registers.h)
 void Gpt_Notification_TIM6( void )
+=======
+void Gpt_Notification_Channel1( void )
+>>>>>>> d5b5122 (GPT-Code-Implementation. Corrections made on the functions.)
 {
     /*Implemented by the user*/
+
+    Bfx_ClrBit_u32u8( (uint32 *)&TIM6->SR, 0 ); /*Clearing the update interrupt flag of TIMx_SR*/
 }
 
-void Gpt_Notification_TIM7( void )
+void Gpt_Notification_Channel2( void )
 {
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -237,5 +208,10 @@ void Gpt_Notification_TIM7( void )
 >>>>>>> 8a205fd (GPT-Code-Implementation. Autoformat runned, fixed some warnings.)
 =======
     /*Implemented by the user*/
+<<<<<<< HEAD
 >>>>>>> bbfe89e (GPT-Code-Implementation. Rest of the functions filled.)
+=======
+
+    Bfx_ClrBit_u32u8( (uint32 *)&TIM7->SR, 0 ); /*Clearing the update interrupt flag of TIMx_SR*/
+>>>>>>> d5b5122 (GPT-Code-Implementation. Corrections made on the functions.)
 }
