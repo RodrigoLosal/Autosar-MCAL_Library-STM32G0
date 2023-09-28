d5b512248d25f4ad493ade4f075264392de6eea0
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-09-28 03:25:43 -0600

    GPT-Code-Implementation. Corrections made on the functions.

--- autosar/mcal/Gpt.h
+++ autosar/mcal/Gpt.h
@@ -1,18 +1,21 @@
 #ifndef GPT_H__
 #define GPT_H__
 
+#define GPT_NUMBER_OF_CHANNELS 2
+
 typedef struct _Gpt_ConfigType
 {
     uint8 Channel;
+    uint8 NotificationMode;
     uint8 ChannelMode;
-    uint8 ChannelState;
     uint16 Prescaler;
+    void ( *Notifications[ 2u ] )( void );
 } Gpt_ConfigType;
 
 typedef enum
 {
-    Channel1 = 0,
-    Channel2,
+    Channel0 = 0,
+    Channel1,
 } Gpt_ChannelType;
 
 typedef enum
@@ -20,6 +23,12 @@ typedef enum
     GPT_CH_MODE_CONTINUOUS = 0,
     GPT_CH_MODE_ONESHOT,
 } Gpt_ChannelMode;
+
+typedef enum
+{
+    Enabled = 0,
+    Disabled,
+} Gpt_NotificationMode;
 
 typedef enum
 {
@@ -40,7 +49,7 @@ void Gpt_StartTimer( Gpt_ChannelType Channel, Gpt_ValueType Value );
 void Gpt_StopTimer( Gpt_ChannelType Channel );
 void Gpt_EnableNotification( Gpt_ChannelType Channel );
 void Gpt_DisableNotification( Gpt_ChannelType Channel );
-void Gpt_Notification_TIM6( void );
-void Gpt_Notification_TIM7( void );
+void Gpt_Notification_Channel1( void );
+void Gpt_Notification_Channel2( void );
 
 #endif
