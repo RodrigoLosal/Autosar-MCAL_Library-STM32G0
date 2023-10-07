6e944fa16990f38ef7baee6a0da625bc8c55358f
Enrique <joenorlu@gmail.com>
2023-10-06 23:05:08 -0600

    Create the Dio_Cfg.h file for some configuration definitions

---
+++ test/support/Dio_Cfg.h
@@ -0,0 +1,10 @@
+#ifndef DIO_CFG_H_
+#define DIO_CFG_H_
+
+#define DIO_VENDOR_ID        (uint16)1000u
+#define DIO_MODULE_ID        (uint16)120u
+#define DIO_SW_MAJOR_VERSION (uint8)1u
+#define DIO_SW_MINOR_VERSION (uint8)0u
+#define DIO_SW_PATCH_VERSION (uint8)0u
+
+#endif /* DIO_CFG_H_ */
