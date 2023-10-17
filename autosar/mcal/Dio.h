cb61568aad892b857b63b42270241b5a5466b2b0
Enrique <joenorlu@gmail.com>
2023-10-16 18:02:16 -0600

    We write the macros for each DET

--- autosar/mcal/Dio.h
+++ autosar/mcal/Dio.h
@@ -85,22 +85,22 @@ typedef struct
   * Symbols to specify the values of the DIO pins on port A, the 4 most significant
   * bits represent the pin port and the 4 least significant bits represent the pin.
   @{ */
-#define DIO_PIN_PA_00 (Dio_ChannelType)0x00u /*!< Dio A pin 0 value */
-#define DIO_PIN_PA_01 (Dio_ChannelType)0x01u /*!< Dio A pin 1 value */
-#define DIO_PIN_PA_02 (Dio_ChannelType)0x02u /*!< Dio A pin 2 value */
-#define DIO_PIN_PA_03 (Dio_ChannelType)0x03u /*!< Dio A pin 3 value */
-#define DIO_PIN_PA_04 (Dio_ChannelType)0x04u /*!< Dio A pin 4 value */
-#define DIO_PIN_PA_05 (Dio_ChannelType)0x05u /*!< Dio A pin 5 value */
-#define DIO_PIN_PA_06 (Dio_ChannelType)0x06u /*!< Dio A pin 6 value */
-#define DIO_PIN_PA_07 (Dio_ChannelType)0x07u /*!< Dio A pin 7 value */
-#define DIO_PIN_PA_08 (Dio_ChannelType)0x08u /*!< Dio A pin 8 value */
-#define DIO_PIN_PA_09 (Dio_ChannelType)0x09u /*!< Dio A pin 9 value */
-#define DIO_PIN_PA_10 (Dio_ChannelType)0x0Au /*!< Dio A pin 10 value */
-#define DIO_PIN_PA_11 (Dio_ChannelType)0x0Bu /*!< Dio A pin 11 value */
-#define DIO_PIN_PA_12 (Dio_ChannelType)0x0Cu /*!< Dio A pin 12 value */
-#define DIO_PIN_PA_13 (Dio_ChannelType)0x0Du /*!< Dio A pin 13 value */
-#define DIO_PIN_PA_14 (Dio_ChannelType)0x0Eu /*!< Dio A pin 14 value */
-#define DIO_PIN_PA_15 (Dio_ChannelType)0x0Fu /*!< Dio A pin 15 value */
+#define DIO_PIN_PA_00                  (Dio_ChannelType)0x00u /*!< Dio A pin 0 value */
+#define DIO_PIN_PA_01                  (Dio_ChannelType)0x01u /*!< Dio A pin 1 value */
+#define DIO_PIN_PA_02                  (Dio_ChannelType)0x02u /*!< Dio A pin 2 value */
+#define DIO_PIN_PA_03                  (Dio_ChannelType)0x03u /*!< Dio A pin 3 value */
+#define DIO_PIN_PA_04                  (Dio_ChannelType)0x04u /*!< Dio A pin 4 value */
+#define DIO_PIN_PA_05                  (Dio_ChannelType)0x05u /*!< Dio A pin 5 value */
+#define DIO_PIN_PA_06                  (Dio_ChannelType)0x06u /*!< Dio A pin 6 value */
+#define DIO_PIN_PA_07                  (Dio_ChannelType)0x07u /*!< Dio A pin 7 value */
+#define DIO_PIN_PA_08                  (Dio_ChannelType)0x08u /*!< Dio A pin 8 value */
+#define DIO_PIN_PA_09                  (Dio_ChannelType)0x09u /*!< Dio A pin 9 value */
+#define DIO_PIN_PA_10                  (Dio_ChannelType)0x0Au /*!< Dio A pin 10 value */
+#define DIO_PIN_PA_11                  (Dio_ChannelType)0x0Bu /*!< Dio A pin 11 value */
+#define DIO_PIN_PA_12                  (Dio_ChannelType)0x0Cu /*!< Dio A pin 12 value */
+#define DIO_PIN_PA_13                  (Dio_ChannelType)0x0Du /*!< Dio A pin 13 value */
+#define DIO_PIN_PA_14                  (Dio_ChannelType)0x0Eu /*!< Dio A pin 14 value */
+#define DIO_PIN_PA_15                  (Dio_ChannelType)0x0Fu /*!< Dio A pin 15 value */
 /**
   @} */
 
@@ -110,22 +110,22 @@ typedef struct
   * Symbols to specify the values of the DIO pins on port B, the 4 most significant
   * bits represent the pin port and the 4 least significant bits represent the pin.
   @{ */
-#define DIO_PIN_PB_00 (Dio_ChannelType)0x10u /*!< Dio B pin 0 value */
-#define DIO_PIN_PB_01 (Dio_ChannelType)0x11u /*!< Dio B pin 1 value */
-#define DIO_PIN_PB_02 (Dio_ChannelType)0x12u /*!< Dio B pin 2 value */
-#define DIO_PIN_PB_03 (Dio_ChannelType)0x13u /*!< Dio B pin 3 value */
-#define DIO_PIN_PB_04 (Dio_ChannelType)0x14u /*!< Dio B pin 4 value */
-#define DIO_PIN_PB_05 (Dio_ChannelType)0x15u /*!< Dio B pin 5 value */
-#define DIO_PIN_PB_06 (Dio_ChannelType)0x16u /*!< Dio B pin 6 value */
-#define DIO_PIN_PB_07 (Dio_ChannelType)0x17u /*!< Dio B pin 7 value */
-#define DIO_PIN_PB_08 (Dio_ChannelType)0x18u /*!< Dio B pin 8 value */
-#define DIO_PIN_PB_09 (Dio_ChannelType)0x19u /*!< Dio B pin 9 value */
-#define DIO_PIN_PB_10 (Dio_ChannelType)0x1Au /*!< Dio B pin 10 value */
-#define DIO_PIN_PB_11 (Dio_ChannelType)0x1Bu /*!< Dio B pin 11 value */
-#define DIO_PIN_PB_12 (Dio_ChannelType)0x1Cu /*!< Dio B pin 12 value */
-#define DIO_PIN_PB_13 (Dio_ChannelType)0x1Du /*!< Dio B pin 13 value */
-#define DIO_PIN_PB_14 (Dio_ChannelType)0x1Eu /*!< Dio B pin 14 value */
-#define DIO_PIN_PB_15 (Dio_ChannelType)0x1Fu /*!< Dio B pin 15 value */
+#define DIO_PIN_PB_00                  (Dio_ChannelType)0x10u /*!< Dio B pin 0 value */
+#define DIO_PIN_PB_01                  (Dio_ChannelType)0x11u /*!< Dio B pin 1 value */
+#define DIO_PIN_PB_02                  (Dio_ChannelType)0x12u /*!< Dio B pin 2 value */
+#define DIO_PIN_PB_03                  (Dio_ChannelType)0x13u /*!< Dio B pin 3 value */
+#define DIO_PIN_PB_04                  (Dio_ChannelType)0x14u /*!< Dio B pin 4 value */
+#define DIO_PIN_PB_05                  (Dio_ChannelType)0x15u /*!< Dio B pin 5 value */
+#define DIO_PIN_PB_06                  (Dio_ChannelType)0x16u /*!< Dio B pin 6 value */
+#define DIO_PIN_PB_07                  (Dio_ChannelType)0x17u /*!< Dio B pin 7 value */
+#define DIO_PIN_PB_08                  (Dio_ChannelType)0x18u /*!< Dio B pin 8 value */
+#define DIO_PIN_PB_09                  (Dio_ChannelType)0x19u /*!< Dio B pin 9 value */
+#define DIO_PIN_PB_10                  (Dio_ChannelType)0x1Au /*!< Dio B pin 10 value */
+#define DIO_PIN_PB_11                  (Dio_ChannelType)0x1Bu /*!< Dio B pin 11 value */
+#define DIO_PIN_PB_12                  (Dio_ChannelType)0x1Cu /*!< Dio B pin 12 value */
+#define DIO_PIN_PB_13                  (Dio_ChannelType)0x1Du /*!< Dio B pin 13 value */
+#define DIO_PIN_PB_14                  (Dio_ChannelType)0x1Eu /*!< Dio B pin 14 value */
+#define DIO_PIN_PB_15                  (Dio_ChannelType)0x1Fu /*!< Dio B pin 15 value */
 /**
   @} */
 
@@ -135,22 +135,22 @@ typedef struct
   * Symbols to specify the values of the DIO pins on port C, the 4 most significant
   * bits represent the pin port and the 4 least significant bits represent the pin.
   @{ */
-#define DIO_PIN_PC_00 (Dio_ChannelType)0x20u /*!< Dio C pin 0 value */
-#define DIO_PIN_PC_01 (Dio_ChannelType)0x21u /*!< Dio C pin 1 value */
-#define DIO_PIN_PC_02 (Dio_ChannelType)0x22u /*!< Dio C pin 2 value */
-#define DIO_PIN_PC_03 (Dio_ChannelType)0x23u /*!< Dio C pin 3 value */
-#define DIO_PIN_PC_04 (Dio_ChannelType)0x24u /*!< Dio C pin 4 value */
-#define DIO_PIN_PC_05 (Dio_ChannelType)0x25u /*!< Dio C pin 5 value */
-#define DIO_PIN_PC_06 (Dio_ChannelType)0x26u /*!< Dio C pin 6 value */
-#define DIO_PIN_PC_07 (Dio_ChannelType)0x27u /*!< Dio C pin 7 value */
-#define DIO_PIN_PC_08 (Dio_ChannelType)0x28u /*!< Dio C pin 8 value */
-#define DIO_PIN_PC_09 (Dio_ChannelType)0x29u /*!< Dio C pin 9 value */
-#define DIO_PIN_PC_10 (Dio_ChannelType)0x2Au /*!< Dio C pin 10 value */
-#define DIO_PIN_PC_11 (Dio_ChannelType)0x2Bu /*!< Dio C pin 11 value */
-#define DIO_PIN_PC_12 (Dio_ChannelType)0x2Cu /*!< Dio C pin 12 value */
-#define DIO_PIN_PC_13 (Dio_ChannelType)0x2Du /*!< Dio C pin 13 value */
-#define DIO_PIN_PC_14 (Dio_ChannelType)0x2Eu /*!< Dio C pin 14 value */
-#define DIO_PIN_PC_15 (Dio_ChannelType)0x2Fu /*!< Dio C pin 15 value */
+#define DIO_PIN_PC_00                  (Dio_ChannelType)0x20u /*!< Dio C pin 0 value */
+#define DIO_PIN_PC_01                  (Dio_ChannelType)0x21u /*!< Dio C pin 1 value */
+#define DIO_PIN_PC_02                  (Dio_ChannelType)0x22u /*!< Dio C pin 2 value */
+#define DIO_PIN_PC_03                  (Dio_ChannelType)0x23u /*!< Dio C pin 3 value */
+#define DIO_PIN_PC_04                  (Dio_ChannelType)0x24u /*!< Dio C pin 4 value */
+#define DIO_PIN_PC_05                  (Dio_ChannelType)0x25u /*!< Dio C pin 5 value */
+#define DIO_PIN_PC_06                  (Dio_ChannelType)0x26u /*!< Dio C pin 6 value */
+#define DIO_PIN_PC_07                  (Dio_ChannelType)0x27u /*!< Dio C pin 7 value */
+#define DIO_PIN_PC_08                  (Dio_ChannelType)0x28u /*!< Dio C pin 8 value */
+#define DIO_PIN_PC_09                  (Dio_ChannelType)0x29u /*!< Dio C pin 9 value */
+#define DIO_PIN_PC_10                  (Dio_ChannelType)0x2Au /*!< Dio C pin 10 value */
+#define DIO_PIN_PC_11                  (Dio_ChannelType)0x2Bu /*!< Dio C pin 11 value */
+#define DIO_PIN_PC_12                  (Dio_ChannelType)0x2Cu /*!< Dio C pin 12 value */
+#define DIO_PIN_PC_13                  (Dio_ChannelType)0x2Du /*!< Dio C pin 13 value */
+#define DIO_PIN_PC_14                  (Dio_ChannelType)0x2Eu /*!< Dio C pin 14 value */
+#define DIO_PIN_PC_15                  (Dio_ChannelType)0x2Fu /*!< Dio C pin 15 value */
 /**
   @} */
 
@@ -160,15 +160,15 @@ typedef struct
   * Symbols to specify the values of the DIO pins on port D, the 4 most significant
   * bits represent the pin port and the 4 least significant bits represent the pin.
   @{ */
-#define DIO_PIN_PD_00 (Dio_ChannelType)0x30u /*!< Dio D pin 0 value */
-#define DIO_PIN_PD_01 (Dio_ChannelType)0x31u /*!< Dio D pin 1 value */
-#define DIO_PIN_PD_02 (Dio_ChannelType)0x32u /*!< Dio D pin 2 value */
-#define DIO_PIN_PD_03 (Dio_ChannelType)0x33u /*!< Dio D pin 3 value */
-#define DIO_PIN_PD_04 (Dio_ChannelType)0x34u /*!< Dio D pin 4 value */
-#define DIO_PIN_PD_05 (Dio_ChannelType)0x35u /*!< Dio D pin 5 value */
-#define DIO_PIN_PD_06 (Dio_ChannelType)0x36u /*!< Dio D pin 6 value */
-#define DIO_PIN_PD_08 (Dio_ChannelType)0x38u /*!< Dio D pin 8 value */
-#define DIO_PIN_PD_09 (Dio_ChannelType)0x39u /*!< Dio D pin 9 value */
+#define DIO_PIN_PD_00                  (Dio_ChannelType)0x30u /*!< Dio D pin 0 value */
+#define DIO_PIN_PD_01                  (Dio_ChannelType)0x31u /*!< Dio D pin 1 value */
+#define DIO_PIN_PD_02                  (Dio_ChannelType)0x32u /*!< Dio D pin 2 value */
+#define DIO_PIN_PD_03                  (Dio_ChannelType)0x33u /*!< Dio D pin 3 value */
+#define DIO_PIN_PD_04                  (Dio_ChannelType)0x34u /*!< Dio D pin 4 value */
+#define DIO_PIN_PD_05                  (Dio_ChannelType)0x35u /*!< Dio D pin 5 value */
+#define DIO_PIN_PD_06                  (Dio_ChannelType)0x36u /*!< Dio D pin 6 value */
+#define DIO_PIN_PD_08                  (Dio_ChannelType)0x38u /*!< Dio D pin 8 value */
+#define DIO_PIN_PD_09                  (Dio_ChannelType)0x39u /*!< Dio D pin 9 value */
 /**
   @} */
 
@@ -178,11 +178,11 @@ typedef struct
   * Symbols to specify the values of the DIO pins on port F, the 4 most significant
   * bits represent the pin port and the 4 least significant bits represent the pin.
   @{ */
-#define DIO_PIN_PF_00 (Dio_ChannelType)0x50u /*!< Dio F pin 0 value */
-#define DIO_PIN_PF_01 (Dio_ChannelType)0x51u /*!< Dio F pin 1 value */
-#define DIO_PIN_PF_02 (Dio_ChannelType)0x52u /*!< Dio F pin 2 value */
-#define DIO_PIN_PF_03 (Dio_ChannelType)0x53u /*!< Dio F pin 3 value */
-#define DIO_PIN_PF_04 (Dio_ChannelType)0x54u /*!< Dio F pin 4 value */
+#define DIO_PIN_PF_00                  (Dio_ChannelType)0x50u /*!< Dio F pin 0 value */
+#define DIO_PIN_PF_01                  (Dio_ChannelType)0x51u /*!< Dio F pin 1 value */
+#define DIO_PIN_PF_02                  (Dio_ChannelType)0x52u /*!< Dio F pin 2 value */
+#define DIO_PIN_PF_03                  (Dio_ChannelType)0x53u /*!< Dio F pin 3 value */
+#define DIO_PIN_PF_04                  (Dio_ChannelType)0x54u /*!< Dio F pin 4 value */
 /**
   @} */
 
@@ -191,13 +191,23 @@ typedef struct
  * Symbols to specify ports values.
  */
 /**@{*/
-#define PORTS_A       0u /*!< Port A value */
-#define PORTS_B       1u /*!< Port B value */
-#define PORTS_C       2u /*!< Port C value */
-#define PORTS_D       3u /*!< Port D value */
-#define PORTS_E       4u /*!< Port E value */
-#define PORTS_F       5u /*!< Port F value */
+#define PORTS_A                        0u /*!< Port A value */
+#define PORTS_B                        1u /*!< Port B value */
+#define PORTS_C                        2u /*!< Port C value */
+#define PORTS_D                        3u /*!< Port D value */
+#define PORTS_E                        4u /*!< Port E value */
+#define PORTS_F                        5u /*!< Port F value */
 /**@}*/
+
+/**
+ * @defgroup DIO_errors DIO Driver Errors
+ * @{ */
+#define DIO_E_PARAM_INVALID_CHANNEL_ID 0x0A /*!< Invalid channel requested */
+#define DIO_E_PARAM_INVALID_PORT_ID    0x14 /*!< Invalid port requested */
+#define DIO_E_PARAM_INVALID_GROUP      0x1F /*!< Invalid channel group requested  */
+#define DIO_E_PARAM_POINTER            0x20 /*!< API service called with a NULL pointer */
+/**
+ * @} */
 
 Dio_LevelType Dio_ReadChannel( Dio_ChannelType ChannelId );
 void Dio_WriteChannel( Dio_ChannelType ChannelId, Dio_LevelType Level );
