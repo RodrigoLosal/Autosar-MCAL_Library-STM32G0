3deafd8b2cfb404a21770ae48068bfaf56c04677
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-10-16 18:47:42 -0600

    Corrected a minor detail in Registers.h

--- autosar/mcal/regs/Registers.h
+++ autosar/mcal/regs/Registers.h
@@ -227,7 +227,7 @@ typedef struct
     volatile uint32 DIER;           /*!< TIM DMA/Interrupt enable register */
     volatile uint32 SR;             /*!< TIM status register */
     volatile uint32 EGR;            /*!< TIM event generation register */
-    volatile uint32 Reserved1[ 3 ]; /*!< Reserved memory space */
+    volatile uint32 Reserved1[ 3u ]; /*!< Reserved memory space */
     volatile uint32 CNT;            /*!< TIM counter */
     volatile uint32 PSC;            /*!< TIM prescaler */
     volatile uint32 ARR;            /*!< TIM auto-reload register */
