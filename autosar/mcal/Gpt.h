7c8a734b5ee395b486a4600493298c8d0b08cb59
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-10-16 18:40:55 -0600

    Solved a compilation error.

--- autosar/mcal/Gpt.h
+++ autosar/mcal/Gpt.h
@@ -1,9 +1,32 @@
+/**
+ * @file    Port.h
+ * @brief   **Gpt driver configuration**
+ * @author  Rodrigo Lopez, Diego Perez
+ *
+ * The GPT driver provides services for starting and stopping timer channels (logical timer
+ * instances assigned to a timer hardware), individual for each channel.
+ *
+ * The tick duration of a timer channel depends on channel specific settings (part of GPT driver)
+ * as well as on system clock and settings of the clock tree controlled by the MCU module.
+ */
 #ifndef GPT_H__
 #define GPT_H__
 
+/**
+ * @defgroup    PORTS_pins Pins values
+ *
+ * Symbols to specify the values of PORTS pins on any port, this needs to
+ * be use in combination with ports when specify in Port_Cfg.h.
+ *
+ * @{ */
 #define SET                    1u
 #define RESET                  0
 
+/**
+ * @defgroup    GPT bits values
+ *
+ * Symbols to specify the values of the bits on the registers.
+ * @{ */
 #define GPT_INTERRUPT_FLAG_BIT 0
 #define GPT_ONE_PULSE_MODE_BIT 3u
 #define GPT_PRESCALER_LSB      0
@@ -13,14 +36,20 @@
 #define GPT_COUNTER_ENABLE_BIT 0
 #define GPT_UPDATE_DISABLE_BIT 1u
 
-#include "Gpt_Cfg.h" /* cppcheck-suppress misra-c2012-20.1 ; it is necesary to use a define for this function */
+/**
+ * @defgroup    GPT channel mode values
+ *
+ * Symbols to specify the values of the possible channel modes.
+ * @{ */
+#define GPT_CH_MODE_CONTINUOUS 0
+#define GPT_CH_MODE_ONESHOT    1u
 
 typedef struct _Gpt_ConfigType
 {
     uint8 GptChannelId;
     uint8 GptChannelMode;
     uint16 GptChannelPrescaler;
-    void ( *GptNotification[ GPT_NUMBER_OF_CHANNELS ] )( void );
+    void ( *GptNotification[ 2u ] )( void );
 } Gpt_ConfigType;
 
 typedef enum
@@ -30,6 +59,8 @@ typedef enum
 } Gpt_ChannelType;
 
 typedef uint32 Gpt_ValueType;
+
+#include "Gpt_Cfg.h" /* cppcheck-suppress misra-c2012-20.1 ; it is necesary to use a define for this function */
 
 void Gpt_Init( const Gpt_ConfigType *ConfigPtr );
 
