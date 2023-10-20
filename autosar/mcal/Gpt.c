0fb3ce7bfc6cd9dcd69182c3f668ddcf84477b12
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-10-19 19:20:28 -0600

    Documentation created.

--- autosar/mcal/Gpt.c
+++ autosar/mcal/Gpt.c
@@ -1,12 +1,41 @@
+/**
+ * @file    Gpt.c
+ * @brief   **Gpt driver configuration**
+ * @author  Rodrigo Lopez, Diego Perez
+ *
+ * The GPT driver provides services for starting and stopping timer channels (logical timer
+ * instances assigned to a timer hardware), individual for each channel.
+ *
+ * The tick duration of a timer channel depends on channel specific settings (part of GPT driver)
+ * as well as on system clock and settings of the clock tree controlled by the MCU module.
+ */
 #include "Bfx.h"
 #include "Std_Types.h"
 #include "Registers.h"
 #include "Gpt.h"
 
+/**
+ * @brief Global GPT register array.
+ */
 static Gpt_RegisterType *channels[ GPT_NUMBER_OF_CHANNELS ] = { TIM6, TIM7 };
 
+/**
+ * @brief  Variable for the initial value of the GPT configuration array.
+ */
 static const Gpt_ConfigType *LocalGptConfigPtr = NULL_PTR;
 
+/**
+ * @brief Initialize the GPT registers to the configuration stored on ConfigPtr.
+ *
+ * The function Gpt_Init shall initialize the hardware timer module according to a configuration
+ * set referenced by ConfigPtr. The functions disables all interrupt notifications.
+ *
+ * Resources that are not configured in the configuration file are not touched.
+ *
+ * @param ConfigPtr       Pointer to ConfigPtr struct array.
+ *
+ * @reqs   SWS_Gpt_00280
+ */
 void Gpt_Init( const Gpt_ConfigType *ConfigPtr )
 {
     Gpt_RegisterType *channel;
@@ -21,6 +50,14 @@ void Gpt_Init( const Gpt_ConfigType *ConfigPtr )
     LocalGptConfigPtr = ConfigPtr;
 }
 
+/**
+ * @brief Deinitializes the GPT driver.
+ *
+ * The function deinitializes the hardware used by the GPT driver (depending on configuration) to
+ * the power on reset state. Values of registers which are not writeable are excluded.
+ *
+ * @reqs   SWS_Gpt_00281
+ */
 #if GPT_DEINIT_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 void Gpt_DeInit( void )
 {
@@ -36,6 +73,18 @@ void Gpt_DeInit( void )
 }
 #endif
 
+/**
+ * @brief Returns the time already elapsed.
+ *
+ * The function returns the time already elapsed. When the channel is in mode “one-shot mode”,
+ * this is the value relative to the point in time, the channel has been started.
+ *
+ * @param Channel       Numeric identifier of the GPT channel.
+ *
+ * @retval  channel->CNT
+ *
+ * @reqs   SWS_Gpt_00282
+ */
 #if GPT_TIME_ELAPSED_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 Gpt_ValueType Gpt_GetTimeElapsed( Gpt_ChannelType Channel )
 {
@@ -47,6 +96,18 @@ Gpt_ValueType Gpt_GetTimeElapsed( Gpt_ChannelType Channel )
 }
 #endif
 
+/**
+ * @brief Returns the time remaining until the target time is reached.
+ *
+ * The function returns the timer value remaining until the target time will be reached next time.
+ * he remaining time is the "target time" minus the time already elapsed.
+ *
+ * @param Channel       Numeric identifier of the GPT channel.
+ *
+ * @retval  channel->ARR - channel->CNT
+ *
+ * @reqs   SWS_Gpt_00282
+ */
 #if GPT_TIME_REMAINING_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 Gpt_ValueType Gpt_GetTimeRemaining( Gpt_ChannelType Channel )
 {
@@ -58,6 +119,17 @@ Gpt_ValueType Gpt_GetTimeRemaining( Gpt_ChannelType Channel )
 }
 #endif
 
+/**
+ * @brief Starts a timer channel.
+ *
+ * The function starts the selected timer channel with a defined target time. If configured and
+ * enabled, an interrupt notification or a wakeup interrupt occurs, when the target time is reached.
+ *
+ * @param Channel       Numeric identifier of the GPT channel.
+ * @param Value         Target time in number of ticks.
+ *
+ * @reqs   SWS_Gpt_00284
+ */
 void Gpt_StartTimer( Gpt_ChannelType Channel, Gpt_ValueType Value )
 {
     Gpt_RegisterType *channel;
@@ -67,6 +139,15 @@ void Gpt_StartTimer( Gpt_ChannelType Channel, Gpt_ValueType Value )
     Bfx_SetBit_u32u8( (uint32 *)&channel->CR1, GPT_COUNTER_ENABLE_BIT );                               /*Setting the CEN: bit of TIMx_CR1*/
 }
 
+/**
+ * @brief Stops a timer channel.
+ *
+ * The function stops the selected timer channel.
+ *
+ * @param Channel       Numeric identifier of the GPT channel.
+ *
+ * @reqs   SWS_Gpt_00285
+ */
 void Gpt_StopTimer( Gpt_ChannelType Channel )
 {
     Gpt_RegisterType *channel;
@@ -74,6 +155,16 @@ void Gpt_StopTimer( Gpt_ChannelType Channel )
     Bfx_ClrBit_u32u8( (uint32 *)&channel->CR1, GPT_COUNTER_ENABLE_BIT ); /*Clearing the CEN: bit of TIMx_CR1*/
 }
 
+/**
+ * @brief Gets the current version.
+ *
+ * The function gives the versioninfo struct the values of the current version. to eneable this
+ * function change the value of GPT_VERSION_INFO_API to STD_ON on the Gpt_Cfg file.
+ *
+ * @param versioninfo             Pointer to Std_VersionInfoType struct.
+ *
+ * @reqs   SWS_Gpt_00279
+ */
 #if GPT_VERSION_INFO_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 void Gpt_GetVersionInfo( Std_VersionInfoType *versioninfo )
 {
@@ -85,6 +176,16 @@ void Gpt_GetVersionInfo( Std_VersionInfoType *versioninfo )
 }
 #endif
 
+/**
+ * @brief Enables the interrupt notification for a channel.
+ *
+ * The function enables the interrupt notification of the referenced channel configured for
+ * notification.
+ *
+ * @param Channel       Numeric identifier of the GPT channel.
+ *
+ * @reqs   SWS_Gpt_00286
+ */
 #if GPT_ENABLE_DISABLE_NOTIFICATION_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 void Gpt_EnableNotification( Gpt_ChannelType Channel )
 {
@@ -94,6 +195,16 @@ void Gpt_EnableNotification( Gpt_ChannelType Channel )
 }
 #endif
 
+/**
+ * @brief Disables the interrupt notification for a channel.
+ *
+ * The function disables the interrupt notification of the referenced channel configured for
+ * notification.
+ *
+ * @param Channel       Numeric identifier of the GPT channel.
+ *
+ * @reqs   SWS_Gpt_00287
+ */
 #if GPT_ENABLE_DISABLE_NOTIFICATION_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 void Gpt_DisableNotification( Gpt_ChannelType Channel )
 {
@@ -103,6 +214,16 @@ void Gpt_DisableNotification( Gpt_ChannelType Channel )
 }
 #endif
 
+/**
+ * @brief Executes the callback function defined by the user on the GPT_CHANNEL_0,
+ * validating the interrupt flag and turning it off afterwards.
+ *
+ * The notification prototype Gpt_Notification_Channel0 is for the notification callback function
+ * and shall be implemented by the user. The callback notifications Gpt_Notification_<channel> shall
+ * be configurable as pointers to user defined functions within the configuration structure.
+ *
+ * @reqs   SWS_Gpt_00292
+ */
 #if GPT_ENABLE_DISABLE_NOTIFICATION_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 void Gpt_Notification_Channel0( void )
 {
@@ -114,6 +235,16 @@ void Gpt_Notification_Channel0( void )
 }
 #endif
 
+/**
+ * @brief Executes the callback function defined by the user on the GPT_CHANNEL_1,
+ * validating the interrupt flag and turning it off afterwards.
+ *
+ * The notification prototype Gpt_Notification_Channel1 is for the notification callback function
+ * and shall be implemented by the user. The callback notifications Gpt_Notification_<channel> shall
+ * be configurable as pointers to user defined functions within the configuration structure.
+ *
+ * @reqs   SWS_Gpt_00292
+ */
 #if GPT_ENABLE_DISABLE_NOTIFICATION_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 void Gpt_Notification_Channel1( void )
 {
