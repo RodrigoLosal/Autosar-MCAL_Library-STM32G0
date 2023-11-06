e2ee5b4e5cfaa3adcf202aa8028ae031fe26313d
Alejandro Ascencio <mascencio1602@gmail.com>
2023-11-06 12:16:40 -0600

    [2023.11.06] New file SPI and documents Spi.c, Spi.h and Spi_Types.h

---
+++ autosar/mcal/Spi/Spi.c
@@ -0,0 +1,26 @@
+/**
+ * @file    Spi.c
+ * @brief   **Spi Driver**
+ * @author  Manuel Alejandro Ascencio Ysordia
+ *
+ * The Can module provides servic ...
+ * ...
+ */
+
+#include "Std_Types.h"
+#include "Spi.h"
+
+/**
+ * @brief    **Spi Initialization**
+ *
+ * This...
+ * ...
+ *
+ * @param    Config Pointer to driver configuration
+ *
+ * @reqs    SWS_Spi_00223
+ */
+void Spi_Init( const Spi_ConfigType *Config )
+{
+    (void)Config;
+}
---
+++ autosar/mcal/Spi/Spi_Types.h
@@ -0,0 +1,17 @@
+#ifndef SPI_TYPES_H__
+#define SPI_TYPES_H__
+
+#include "Registers.h"
+
+/**
+ * @brief **Hardware unit configuration structure**
+ *
+ * This structure contains the paramters to configure the hardware unit incuding the controllers and the
+ * hardware objects
+ */
+typedef struct _Spi_ConfigType
+{
+    uint32 dummy; /*!< dummy element for the moment */
+} Spi_ConfigType;
+
+#endif /* SPI_TYPES_H__ */
