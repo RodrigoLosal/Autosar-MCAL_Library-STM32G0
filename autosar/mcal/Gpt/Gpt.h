62785c52cf0225c68227bdd15783e5634bb8eec3
Rodrigo Lopez <rodrigo.losal@gmail.com>
2023-11-09 17:59:45 -0600

    Refactor made.

--- autosar/mcal/Gpt/Gpt.h
+++ autosar/mcal/Gpt/Gpt.h
@@ -36,44 +36,6 @@
 /**
  * @} */
 
-/**
- * @brief **Definition of the external data structure containing the initialization data**
- *
- * This is the type of the data structure including the configuration set required for
- * initializing the GPT timer unit.
- *
- * @note It is the responsibility of the user to ensure that the values defined in this structure are
- * valid and appropriate, as the GPT Driver does not perform any validation. The values must be generated
- * and validated within the code generation tool.
- *
- * @reqs   SWS_Gpt_00357
- */
-typedef struct _Gpt_ConfigType
-{
-    uint8 GptChannelId;                      /*!< Gpt Channel to be configured */
-    uint8 GptChannelMode;                    /*!< Continuous or One-Pulse mode*/
-    uint16 GptChannelPrescaler;              /*!< Prescaler from 0x0 to 0xFFFF*/
-    void ( *GptNotification[ 2u ] )( void ); /*!< Array of Pointers to user-defined functions*/
-} Gpt_ConfigType;
-
-/**
- * @brief **Numeric ID of a GPT channel**
- *
- * @reqs   SWS_Gpt_00358
- */
-typedef enum
-{
-    GPT_CHANNEL_0 = 0,
-    GPT_CHANNEL_1,
-} Gpt_ChannelType;
-
-/**
- * @brief **Type for reading and setting the timer values (in number of ticks).**
- *
- * @reqs   SWS_Gpt_00359
- */
-typedef uint32 Gpt_ValueType;
-
 #include "Gpt_Cfg.h" /* cppcheck-suppress misra-c2012-20.1 ; it is necesary to use a define for this function */
 
 void Gpt_Init( const Gpt_ConfigType *ConfigPtr );
