0550be47e0bf9acfa3c085fe1e6ed4efc46a81d8
OSCAR gonzalez <oscamon2706@gmail.com>
2023-11-09 21:36:09 -0600

    Adding stub functions

---
+++ autosar/mcal/Det/Det.c
@@ -0,0 +1,140 @@
+/**
+ * @file    Det.h
+ * @brief   **DET driver configuration**
+ * @author  Oscar Gonzalez
+ *
+ */
+
+#include "Det.h"
+#include "Det_Types.h"
+
+/**
+ * @brief    **Det Initialization**
+ *
+ * Service to initialize the Default Error Tracer.
+ *
+ * @param    ConfigPtr 	Pointer to the selected configuration set.
+ *
+ * @reqs    SWS_Det_00008
+ */
+void Det_Init ( const Det_ConfigType* ConfigPtr )
+{
+    (void)ConfigPtr;
+}
+
+/**
+ * @brief    **Det Report Error**
+ *
+ * Service to report development errors.
+ *
+ * @param   Module ID of calling module.
+ * @param   InstanceId The identifier of the index based instance of a module, starting from 0, 
+ * If the module is a single instance module it shall pass 0 as the InstanceId.
+ * @param   ApiId ID of API service in which error is detected (defined in SWS of calling module).
+ * @param   ErrorId ID of detected development error (defined in SWS of calling module).
+ * 
+ * @retval Std_ReturnType: never returns a value, but has a return type for compatibility with services and hooks.
+ * 
+ * @note Det_ReportError may be callable in interrupt context. Since the DET can be called in normal mode or in interrupt context
+ * from stack or integration) this has to be considered during implementation of the hook functions: 
+ * Det_ReportError can be called in interrupt context; this should be considered when halting the system.
+ * 
+ * @reqs    SWS_Det_00009
+ */
+Std_ReturnType Det_ReportError ( uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId )
+{
+    (void)ModuleId;
+    (void)InstanceId;
+    (void)ApiId;
+    (void)ErrorId;
+
+    return;
+}
+
+/**
+ * @brief    **Det Start**
+ *
+ * Service to initialize the Default Error Tracer.
+ *
+ * @reqs    SWS_Det_00010
+ */
+void Det_Start( void )
+{
+}
+
+/**
+ * @brief    **Det Report Runtime Error**
+ *
+ * Service to report runtime errors. If a callout has been configured then this callout shall be called.
+ * 
+ * @param   Module ID of calling module.
+ * @param   InstanceId The identifier of the index based instance of a module, starting from 0, 
+ * If the module is a single instance module it shall pass 0 as the InstanceId.
+ * @param   ApiId ID of API service in which error is detected (defined in SWS of calling module).
+ * @param   ErrorId ID of detected development error (defined in SWS of calling module).
+ * 
+ * @retval Std_ReturnType: returns always E_OK (is required for services)
+ * 
+ * @note Det_ReportRuntime Error may be callable in interrupt context. Since the DET can be called in normal mode or in interrupt 
+ * context (from stack or integration) this has to be considered during implementation of the hook functions: 
+ * Det_ReportRuntimeError can be called in interrupt context; this hook should be reentrant and sufficiently performant.
+ *
+ * @reqs    SWS_Det_01001
+ */
+Std_ReturnType Det_ReportRuntimeError( uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId )
+{
+    (void)ModuleId;
+    (void)InstanceId;
+    (void)ApiId;
+    (void)ErrorId;
+
+    return E_OK;
+}
+
+/**
+ * @brief    **Det Report Transient Fault**
+ *
+ * Service to report transient faults. If a callout has been configured than this callout shall be called 
+ * and the returned value of the callout shall be returned. Otherwise it returns immediately with E_OK.
+ *
+ * @param   Module ID of calling module.
+ * @param   InstanceId The identifier of the index based instance of a module, starting from 0, 
+ * If the module is a single instance module it shall pass 0 as the InstanceId.
+ * @param   ApiId ID of API service in which error is detected (defined in SWS of calling module).
+ * @param   FaultId ID of detected transient fault (defined in SWS of calling module).
+ * 
+ * @retval  If no callout exists it shall return E_OK, otherwise it shall return the value of the configured callout. 
+ * In case several callouts are configured the logical or (sum) of the callout return values shall be returned.
+ * Rationale: since E_OK=0, E_OK will be only returned if all are E_OK, and for multiple error codes there is a good 
+ * chance to detect several of them.
+ * 
+ * @note Det_ReportTransient Fault may be callable in interrupt context. Since the DET can be called in normal mode or in interrupt 
+ * context (from stack or integration) this has to be considered during implementation of the hook functions:
+ * Det_ReportTransientFault can be called in interrupt context; this hook should be reentrant and sufficiently performant.
+ * 
+ * 
+ * @reqs    SWS_Det_01003
+ */
+Std_ReturnType Det_ReportTransientFault (uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 FaultId )
+{
+    (void)ModuleId;
+    (void)InstanceId;
+    (void)ApiId;
+    (void)FaultId;
+
+    return E_OK;
+}
+
+/**
+ * @brief    **Det Get Version Info**
+ *
+ * Returns the version information of this module.
+ * 
+ * @param versioninfo 	Pointer to where to store the version information of this module.
+ *
+ * @reqs    SWS_Det_00011
+ */
+void Det_GetVersionInfo( Std_VersionInfoType* versioninfo )
+{
+    (void)versioninfo;
+}
---
+++ autosar/mcal/Det/Det_Types.h
@@ -0,0 +1,29 @@
+/**
+ * @file    Det.h
+ * @brief   **DET driver configuration**
+ * @author  Oscar Gonzalez
+ * 
+ * The header contains the type definitions that are used by the Det module. The file is implemented
+ * as a means of abstraction from the hardware, making the low level interfaces available for upper layers.
+ *
+ */
+
+#ifndef DET_TYPES_H__
+#define DET_TYPES_H__
+
+#include "Registers.h"
+
+/**
+ * @brief **Det Config type**
+ *
+ * 	Configuration data structure of the Det module
+ *
+ * @reqs SWS_Pwm_00111
+ */
+typedef struct _Det_ConfigType
+{
+    uint32 dummy; /*!< dummy element for the moment */
+} Det_ConfigType;
+
+
+#endif
