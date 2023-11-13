deee4b3a4e8299183f97bf918ff99fa72de329a2
ChristopherBogarin <christopher.bogarint@gmail.com>
2023-11-13 17:14:08 -0600

    Update the if state with STD_ON

--- autosar/mcal/Fls/Fls.h
+++ autosar/mcal/Fls/Fls.h
@@ -70,7 +70,7 @@
 void Fls_Init( const Fls_ConfigType *ConfigPtr );
 Std_ReturnType Fls_Erase( Fls_AddressType TargetAddress, Fls_LengthType Length );
 Std_ReturnType Fls_Write( Fls_AddressType TargetAddress, const uint8 *SourceAddressPtr, Fls_LengthType Length );
-#if FLS_CANCEL_API == STD_ON/* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
+#if FLS_CANCEL_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
 void Fls_Cancel( void );
 #endif
 #if FLS_GET_STATUS_API == STD_ON /* cppcheck-suppress misra-c2012-20.9 ; it is necesary to use a define for this function */
