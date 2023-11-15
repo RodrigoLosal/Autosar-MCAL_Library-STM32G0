95db04959181d999e3e2f9a927906a7ef539ec72
Angel Lozano <angel_ivan_5@outlook.com>
2023-11-14 19:33:50 -0600

    Moved Mcu API service ID and Error types from Mcu.h to Mcu_Types.h

--- autosar/mcal/Mcu/Mcu_Types.h
+++ autosar/mcal/Mcu/Mcu_Types.h
@@ -14,6 +14,39 @@
 #include "Std_Types.h"
 
 /**
+ * @defgroup MCU_Ids MCU Id number for module and each API service
+ *
+ * @{ */
+#define MCU_ID_INIT                 0x00u /*!< Mcu_Init() api service id */
+#define MCU_ID_INIT_RAM             0x01u /*!< Mcu_InitRamSection() api service id */
+#define MCU_ID_INIT_CLOCK           0x02u /*!< Mcu_InitClock() api service id */
+#define MCU_ID_DISTRIBUTE_PLL_CLOCK 0x03u /*!< Mcu_DistributePllClock() api service id */
+#define MCU_ID_GET_PLL_STATUS       0x04u /*!< Mcu_GetPllStatus() api service id */
+#define MCU_ID_GET_RESET_REASON     0x05u /*!< Mcu_GetResetReason() api service id */
+#define MCU_ID_GET_RESET_RAW_VALUE  0x06u /*!< Mcu_GetResetRawValue() api service id */
+#define MCU_ID_PERFORM_RESET        0x07u /*!< Mcu_PerformReset() api service id */
+#define MCU_ID_SET_MODE             0x08u /*!< Mcu_SetMode() api service id */
+#define MCU_ID_GET_VERSION_INFO     0x09u /*!< Mcu_GetVersionInfo() api service id */
+#define MCU_ID_GET_RAM_STATE        0x0Au /*!< Mcu_GetRamState() api service id */
+/**
+ * @} */
+
+/**
+ * @defgroup MCU_Error_Type MCU Development Error Types
+ *
+ * @{ */
+#define MCU_E_PARAM_CONFIG          0x0Au /*!< API service called with wrong parameter */
+#define MCU_E_PARAM_CLOCK           0x0Bu /*!< API service called with wrong parameter */
+#define MCU_E_PARAM_MODE            0x0Cu /*!< API service called with wrong parameter */
+#define MCU_E_PARAM_RAMSECTION      0x0Du /*!< API service called with wrong parameter */
+#define MCU_E_PLL_NOT_LOCKED        0x0Eu /*!< API service called with wrong parameter */
+#define MCU_E_UNINIT                0x0Fu /*!< API service called with wrong parameter */
+#define MCU_E_PARAM_POINTER         0x10u /*!< API service called with wrong parameter */
+#define MCU_E_INIT_FAILED           0x11u /*!< API service called with wrong parameter */
+/**
+ * @} */
+
+/**
  * @brief **Hardware dependent structure**
  *
  * A pointer to this type is provided to MCU initialization routines for configuration
