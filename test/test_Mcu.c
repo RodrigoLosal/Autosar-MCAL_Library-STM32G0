26e344b68efb0acd23e4bafbfab2cf007cb32f80
Angel Lozano <angel_ivan_5@outlook.com>
2023-11-21 21:59:52 -0600

    Two functions testing

--- test/test_Mcu.c
+++ test/test_Mcu.c
@@ -12,9 +12,13 @@
 #include "mock_Mcu_Arch.h"
 #include "mock_Det.h"
 
+extern Mcu_HwUnit HwUnit_Mcu;
+
 /*this function is required by Ceedling to run any code before the test cases*/
 void setUp( void )
 {
+    HwUnit_Mcu.HwUnitState = MCU_STATE_UNINIT;
+    HwUnit_Mcu.Config      = NULL_PTR;
 }
 
 /*this function is required by Ceedling to run any code after the test cases*/
@@ -23,16 +27,53 @@ void tearDown( void )
 }
 
 /**
- * @brief   **Test Init when not CAN_CS_UNINIT**
+ * @brief   **Test Init**
  *
- * The test checks that the function does not call the Can_Arch_Init function and HwUnit structure
- * does not set to CAN_CS_READY, plus Config pointer is not set.
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
  */
-void test__Mcu_Init__myTest( void )
+void test__Mcu_Init__run_for_first_time( void )
 {
-    uint16 var;
+    Mcu_ConfigType McuConfigTest = { 0 };
 
-    var = 15;
+    Mcu_Arch_Init_Ignore( );
+    Mcu_Init( &McuConfigTest );
 
-    TEST_ASSERT_EQUAL_MESSAGE( var, 15, "Expected 15 heheheha" );
+    TEST_ASSERT_EQUAL_MESSAGE( HwUnit_Mcu.HwUnitState, MCU_STATE_INIT, "Expected HwUnitState changed to MCU_STATE_INIT(1)" );
+    TEST_ASSERT_EQUAL_MESSAGE( HwUnit_Mcu.Config, &McuConfigTest, "Expected Config points to McuConfigTest" );
+}
+
+/**
+ * @brief   **Test InitRamSection**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_InitRamSection__run_before_Mcu_init( void )
+{
+    Mcu_RamSectionType RamSectionTest = 0;
+
+    Det_ReportError_IgnoreAndReturn( E_OK );
+    Mcu_InitRamSection( RamSectionTest );
+}
+
+/**
+ * @brief   **Test InitRamSection**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_InitRamSection__run_after_Mcu_init( void )
+{
+    Mcu_ConfigType McuConfigTest      = { 0 };
+    Mcu_RamSectionType RamSectionTest = 0;
+    Std_ReturnType ReturnValue        = E_NOT_OK;
+
+    Mcu_Arch_Init_Ignore( );
+    Mcu_Init( &McuConfigTest );
+
+    Mcu_Arch_InitRamSection_IgnoreAndReturn( E_OK );
+    ReturnValue = Mcu_InitRamSection( RamSectionTest );
+
+    TEST_ASSERT_EQUAL_MESSAGE( ReturnValue, E_OK, "Expected E_OK (0)" );
 }
