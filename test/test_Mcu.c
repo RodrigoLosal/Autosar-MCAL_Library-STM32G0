92e46f1b239c5887c105d1b89e3bb9086b770233
Angel Lozano <angel_ivan_5@outlook.com>
2023-11-22 20:02:07 -0600

    Added test cases

--- test/test_Mcu.c
+++ test/test_Mcu.c
@@ -77,3 +77,165 @@ void test__Mcu_InitRamSection__run_after_Mcu_init( void )
 
     TEST_ASSERT_EQUAL_MESSAGE( ReturnValue, E_OK, "Expected E_OK (0)" );
 }
+
+/**
+ * @brief   **Test Init Clock**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_InitClock__run_before_Mcu_init( void )
+{
+    Mcu_ClockType ClockSettingTest = 0;
+
+    Det_ReportError_IgnoreAndReturn( E_OK );
+    Mcu_InitClock( ClockSettingTest );
+}
+
+/**
+ * @brief   **Test Init Clock**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_InitClock__run_after_Mcu_init( void )
+{
+    Mcu_ConfigType McuConfigTest   = { 0 };
+    Mcu_ClockType ClockSettingTest = 0;
+    Std_ReturnType ReturnValue     = E_NOT_OK;
+
+    Mcu_Arch_Init_Ignore( );
+    Mcu_Init( &McuConfigTest );
+
+    Mcu_Arch_InitClock_IgnoreAndReturn( E_OK );
+    ReturnValue = Mcu_InitClock( ClockSettingTest );
+
+    TEST_ASSERT_EQUAL_MESSAGE( ReturnValue, E_OK, "Expected E_OK (0)" );
+}
+
+//////////Space for DistributePllClock
+
+/**
+ * @brief   **Test Get Reset Reason**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_GetResetReason__run_before_Mcu_init( void )
+{
+    Det_ReportError_IgnoreAndReturn( E_OK );
+    Mcu_GetResetReason( );
+}
+
+/**
+ * @brief   **Test Get Reset Reason**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_GetResetReason__run_after_Mcu_init( void )
+{
+    Mcu_ConfigType McuConfigTest = { 0 };
+    Std_ReturnType ReturnValue   = E_NOT_OK;
+
+    Mcu_Arch_Init_Ignore( );
+    Mcu_Init( &McuConfigTest );
+
+    Mcu_Arch_GetResetReason_IgnoreAndReturn( E_OK );
+    ReturnValue = Mcu_GetResetReason( );
+
+    TEST_ASSERT_EQUAL_MESSAGE( ReturnValue, E_OK, "Expected E_OK (0)" );
+}
+
+/**
+ * @brief   **Test Get Reset Raw Value**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_GetResetRawValue__run_before_Mcu_init( void )
+{
+    Det_ReportError_IgnoreAndReturn( E_OK );
+    Mcu_GetResetRawValue( );
+}
+
+/**
+ * @brief   **Test Get Reset Raw Value**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_GetResetRawValue__run_after_Mcu_init( void )
+{
+    Mcu_ConfigType McuConfigTest = { 0 };
+    Std_ReturnType ReturnValue   = E_NOT_OK;
+
+    Mcu_Arch_Init_Ignore( );
+    Mcu_Init( &McuConfigTest );
+
+    Mcu_Arch_GetResetRawValue_IgnoreAndReturn( E_OK );
+    ReturnValue = Mcu_GetResetRawValue( );
+
+    TEST_ASSERT_EQUAL_MESSAGE( ReturnValue, E_OK, "Expected E_OK (0)" );
+}
+
+/**
+ * @brief   **Test Perform Reset**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_PerformReset__run_before_Mcu_init( void )
+{
+    Det_ReportError_IgnoreAndReturn( E_OK );
+    Mcu_PerformReset( );
+}
+
+/**
+ * @brief   **Test Perform Reset**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_PerformReset__run_after_Mcu_init( void )
+{
+    Mcu_ConfigType McuConfigTest = { 0 };
+
+    Mcu_Arch_Init_Ignore( );
+    Mcu_Init( &McuConfigTest );
+
+    Mcu_Arch_PerformReset_Ignore( );
+    Mcu_PerformReset( );
+}
+
+/**
+ * @brief   **Test Set Mode**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_SetMode__run_before_Mcu_init( void )
+{
+    Mcu_ModeType McuModeTest = 0;
+
+    Det_ReportError_IgnoreAndReturn( E_OK );
+    Mcu_SetMode( McuModeTest );
+}
+
+/**
+ * @brief   **Test Set Mode**
+ *
+ * The test runs Init function, calling Arch_Init and checking that HwUnitState of the HwUnit
+ * struct changes by MCU_STATE_INIT (value 1) and Config of HwUnit points to the passed parameter
+ */
+void test__Mcu_SetMode__run_after_Mcu_init( void )
+{
+    Mcu_ConfigType McuConfigTest = { 0 };
+    Mcu_ModeType McuModeTest     = 0;
+
+    Mcu_Arch_Init_Ignore( );
+    Mcu_Init( &McuConfigTest );
+
+    Mcu_Arch_SetMode_Ignore( );
+    Mcu_SetMode( McuModeTest );
+}
