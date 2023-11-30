c167a89eebcb0674e719f6b133484ec75e93e46b
DiegoMX <diego.perez@modular-mx.com>
2023-11-29 17:59:58 -0700

    set test file empty

---
+++ test/test_CanIf.c
@@ -0,0 +1,23 @@
+/**
+ * @file    test_Can.c
+ * @brief   **Unit testing for Can Driver**
+ * @author  Diego Perez
+ *
+ * group of unit test cases for Can.h file
+ */
+#include "unity.h"
+#include "CanIf_Cfg.h"
+#include "CanIf.h"
+#include "mock_Det.h"
+
+
+/*this function is required by Ceedling to run any code before the test cases*/
+void setUp( void )
+{
+    
+}
+
+/*this function is required by Ceedling to run any code after the test cases*/
+void tearDown( void )
+{
+}
